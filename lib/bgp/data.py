d4703469c40d3f234dfbeee30ef6616da0e9fbe0
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2009-09-06 00:12:13 +0000

    remove unused function

--- lib/bgp/data.py
+++ lib/bgp/data.py
@@ -15,9 +15,6 @@ from struct import pack,unpack
 	# !L : Network Long  (4 bytes)
 	# !H : Network Short (2 bytes)
 
-def dump (data):
-	print [hex(ord(c)) for c in data]
-
 class IP (long):
 	regex_ipv4 = '(?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])\.(?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])\.(?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])\.(?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])'
 
