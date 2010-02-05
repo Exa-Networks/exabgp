572a190ec3e9874fed141796bc67315ea85a6b5b
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2010-02-05 22:55:37 +0000

    another bug found by lint

--- lib/bgp/rib/table.py
+++ lib/bgp/rib/table.py
@@ -61,7 +61,7 @@ class Table (object):
 		for prefix in self._plus.keys():
 			t,p = self._plus[prefix]
 			if t < when:
-				del self._plus[k]
+				del self._plus[prefix]
 		for prefix in self._minus.keys():
 			t = self._minus[prefix]
 			if t < when:
