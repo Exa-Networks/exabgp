95afe457d7becf9980cabff5ab6b4468d00fd356
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2011-03-29 14:48:33 +0100

    adding pdb helpers

---
+++ lib/bgp/debug.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+debug = os.environ.get('DEBUG_EXIT',None)
+
+if debug is None:
+	def intercept (type, value, trace):
+		import traceback
+		print >> sys.stderr, 'the program failed :', value
+	sys.excepthook = intercept
+elif debug not in ['0','']:
+	def intercept (type, value, trace):
+		import traceback
+		import pdb
+		traceback.print_exception(type,value,trace)
+		print
+		pdb.pm()
+	sys.excepthook = intercept
+
+del sys.argv[0]
+
+if sys.argv:
+	__file__ = os.path.abspath(sys.argv[0])
+	__name__ = '__main__'
+	execfile(sys.argv[0])
