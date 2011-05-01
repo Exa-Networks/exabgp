3c8759c58fe302c7b393e83078524d1587214f55
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2011-05-01 17:54:51 +0100

    make the example for the first watchdog feature clearer

--- etc/watchdog/service-1.sh
+++ etc/watchdog/service-1.sh
@@ -1,12 +1,36 @@
 #!/bin/sh
 
 # ignore Control C
-trap '' 2
+# if the user ^C exabgp we will get that signal too, ignore it and let exabgp send us a SIGTERM
+trap '' SIGINT
+
+# command and watchdog name are case sensitive
 
 while `true`;
 do
-echo "announce watchdog watchdog-one"
+
+# Let give exabgp the time to setup the BGP session :)
+# But we do not have too, exabgp will record the changes and update the routes once up otherwise
+
+sleep 10
+
+# without name exabgp will use the name of the service as watchdog name
+echo "withdraw watchdog"
 sleep 5
+
+# specify a watchdog name (which may be the same or different each time)
 echo "withdraw watchdog watchdog-one"
 sleep 5
+
+echo "announce watchdog"
+sleep 5
+
+echo "announce watchdog watchdog-one"
+sleep 5
+
+# we have no route with that watchdog but it does not matter, we could have after a configuration reload
+
+echo "announce watchdog watchdog-two"
+echo "withdraw watchdog watchdog-two"
+
 done
