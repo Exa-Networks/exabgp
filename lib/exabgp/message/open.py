0d46ccb77b732530f0a7e9412acdca0efb9df2e9
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2012-07-10 20:04:08 +0100

    do not announce path-info for family you do not announce in MP

--- lib/exabgp/message/open.py
+++ lib/exabgp/message/open.py
@@ -295,9 +295,12 @@ class Capabilities (dict):
 
 		if neighbor.add_path:
 			ap_families = []
-			ap_families.append((AFI(AFI.ipv4),SAFI(SAFI.unicast)))
-			ap_families.append((AFI(AFI.ipv4),SAFI(SAFI.nlri_mpls)))
-			#ap_families.append((AFI(AFI.ipv6),SAFI(SAFI.unicast)))
+			if (AFI(AFI.ipv4),SAFI(SAFI.unicast)) in families:
+				ap_families.append((AFI(AFI.ipv4),SAFI(SAFI.unicast)))
+			if (AFI(AFI.ipv4),SAFI(SAFI.nlri_mpls)) in families:
+				ap_families.append((AFI(AFI.ipv4),SAFI(SAFI.nlri_mpls)))
+			#if (AFI(AFI.ipv6),SAFI(SAFI.unicast)) in families:
+			#	ap_families.append((AFI(AFI.ipv6),SAFI(SAFI.unicast)))
 			self[Capabilities.ADD_PATH] = AddPath(ap_families,neighbor.add_path)
 
 		if graceful:
