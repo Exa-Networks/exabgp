44611c60affc3592f45e458c77466875778b9a28
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2012-07-18 18:07:24 +0100

    fix bad space in included code and convert all spaces to tabs

--- lib/exabgp/leak/gcdump.py
+++ lib/exabgp/leak/gcdump.py
@@ -3,48 +3,48 @@ import gc
 import inspect
 
 def dump():
-    # force collection
-    print "\nCollecting GARBAGE:"
-    gc.collect()
-    # prove they have been collected
-    print "\nCollecting GARBAGE:"
-    gc.collect()
+	# force collection
+	print "\nCollecting GARBAGE:"
+	gc.collect()
+	# prove they have been collected
+	print "\nCollecting GARBAGE:"
+	gc.collect()
 
-    print "\nGARBAGE OBJECTS:"
-    for x in gc.garbage:
-        s = str(x)
-        if len(s) > 80: s = "%s..." % s[:80]
+	print "\nGARBAGE OBJECTS:"
+	for x in gc.garbage:
+		s = str(x)
+		if len(s) > 80: s = "%s..." % s[:80]
 
-        print "::", s
-        print "        type:", type(x)
-        print "   referrers:", len(gc.get_referrers(x))
-        try:
-            print "    is class:", inspect.isclass(type(x))
-            print "      module:", inspect.getmodule(x)
+		print "::", s
+		print "		type:", type(x)
+		print "   referrers:", len(gc.get_referrers(x))
+		try:
+			print "	is class:", inspect.isclass(type(x))
+			print "	  module:", inspect.getmodule(x)
 
-            lines, line_num = inspect.getsourcelines(type(x))
-            print "    line num:", line_num
-            for l in lines:
-                print "        line:", l.rstrip("\n")
-        except:
-            pass
+			lines, line_num = inspect.getsourcelines(type(x))
+			print "	line num:", line_num
+			for l in lines:
+				print "		line:", l.rstrip("\n")
+		except:
+			pass
 
-        print
+		print
 
 class tmp(object):
-    def __init__(self):
-        a = 0
+	def __init__(self):
+		a = 0
 
 if __name__=="__main__":
- import gc
-    gc.enable()
-    gc.set_debug(gc.DEBUG_LEAK)
+	import gc
+	gc.enable()
+	gc.set_debug(gc.DEBUG_LEAK)
 
-    # make a leak
-    l = [tmp()]
-    l.append(l)
-    del l
+	# make a leak
+	l = [tmp()]
+	l.append(l)
+	del l
 
-    dump_garbage()
+	dump_garbage()
 
 
