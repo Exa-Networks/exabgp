db6e19bed5967e6a79388526b6628a701230d603
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2012-08-28 12:38:41 +0100

    tidy print

--- lib/exabgp/bgp/connection.py
+++ lib/exabgp/bgp/connection.py
@@ -162,7 +162,7 @@ class Connection (object):
 			if not r:
 				# The socket was closed - no data is available anymore (the caller will call .close() on us)
 				raise Failure('The TCP connection is closed')
-			self.logger.wire(LazyFormat("%15s RECV " % self.peer,dump,r))
+			self.logger.wire(LazyFormat("Peer %15s RECV " % self.peer,dump,r))
 			return r
 		except socket.timeout,e:
 			self.close()
@@ -180,7 +180,7 @@ class Connection (object):
 		if not self.ready():
 			return False
 		try:
-			self.logger.wire(LazyFormat("%15s SENT " % self.peer,dump,data))
+			self.logger.wire(LazyFormat("Peer %15s SENT " % self.peer,dump,data))
 			# we can not use sendall as in case of network buffer filling
 			# it does raise and does not let you know how much was sent
 			sent = 0
