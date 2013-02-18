a7bf01dd8c977f380a3192c821062d386cc1eaef
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2013-02-18 09:07:06 +0000

    ignore EINTR (no logging)

--- lib/exabgp/structure/processes.py
+++ lib/exabgp/structure/processes.py
@@ -133,7 +133,9 @@ class Processes (object):
 								self._start(process)
 								break
 					except IOError,e:
-						if e.errno != errno.EAGAIN: # no more data
+						if e.errno == errno.EINTR: # call interrupted
+							pass # we most likely have data, we will try to read them a the next loop iteration
+						elif e.errno != errno.EAGAIN: # no more data
 							self.logger.processes("unexpected errno received from forked process: %d [%s]" % (e.errno,errno.errorcode[e.errno]))
 			except (subprocess.CalledProcessError,OSError,ValueError):
 				self.logger.processes("Issue with the process, terminating it and restarting it")
