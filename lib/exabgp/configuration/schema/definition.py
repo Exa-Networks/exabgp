eb06c9799719782a59436beed76e8b1f14986347
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2013-03-21 20:07:20 +0000

    more slow progress

--- lib/exabgp/configuration/schema/definition.py
+++ lib/exabgp/configuration/schema/definition.py
@@ -38,31 +38,25 @@ PRESENCE = Enumeration(
 	'mandatory'
 )
 
-def check_boolean (data,root,keys):
+# TYPE CHECK
+def check_boolean (data):
 	return type(data) == type(True)
-def check_int8 (data,root,keys):
+def check_int8 (data):
 	return type(data) == type(0) and data < pow(2,8)
-def check_int16 (data,root,keys):
+def check_int16 (data):
 	return type(data) == type(0) and data < pow(2,16)
-def check_int32 (data,root,keys):
+def check_int32 (data):
 	return type(data) == type(0) and data < pow(2,32)
-def check_string (data,root,keys):
+def check_string (data):
 	return type(data) == type(u'')
-def check_list (data,root,keys):
+def check_list (data):
 	return type(data) == type([])
-def check_dict (data,root,keys):
+def check_dict (data):
 	return type(data) == type({})
-def check_ip (data,root,keys):
-	return check_ipv4(data,root,keys) or check_ipv6(data,root,key)
-def check_ipv4 (data,root,keys):  # XXX: improve
-	return type(data) == type(u'') and data.count('.') == 3
-def check_ipv6 (data,root,keys):  # XXX: improve
-	return type(data) == type(u'') and ':' in data
-def check_reference (data,root,keys):
+def check_reference (data):
 	return True
 
-
-CHECK = {
+CHECK_TYPE = {
 	TYPE.boolean : check_boolean,
 	TYPE.int8 : check_int8,
 	TYPE.int16 : check_int16,
@@ -70,11 +64,31 @@ CHECK = {
 	TYPE.string : check_string,
 	TYPE.list : check_list,
 	TYPE.dictionary : check_dict,
-	TYPE.ip : check_ip,
-	TYPE.ipv4 : check_ipv4,
-	TYPE.ipv6 : check_ipv6,
 	TYPE.reference : check_reference,
 }
+
+# DATA CHECK
+def check_nop (data):
+	return True
+def check_ip (data,):
+	return check_ipv4(data) or check_ipv6(data)
+def check_ipv4 (data):  # XXX: improve
+	return type(data) == type(u'') and data.count('.') == 3
+def check_ipv6 (data):  # XXX: improve
+	return type(data) == type(u'') and ':' in data
+def check_positive (data):
+	return data >= 0
+def check_asn (data):
+	return data >= 1
+
+# LIST DATA CHECK
+def check_aspath (data):
+	return type(data) == type(0) and data < pow(2,32)
+def check_community (data):
+	return type(data) == type([]) and len(data) == 2 and type(data[0]) == type(0) and type(data[1]) == type(0)
+
+def check_flow_port (data):
+	return True
 
 class OrderedDict (dict):
 	def __init__(self, args):
@@ -104,20 +118,20 @@ definition = (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
 	('exabgp' , (TYPE.int8, PRESENCE.mandatory, [4,])),
 	('neighbor' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
 		('tcp' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-			('local' , (TYPE.ip, PRESENCE.mandatory , None)),
-			('peer' , (TYPE.ip, PRESENCE.mandatory , None)),
-			('ttl-security' , (TYPE.boolean, PRESENCE.optional , None)),
-			('md5' , (TYPE.string, PRESENCE.optional , None))
+			('local' , (TYPE.string, PRESENCE.mandatory , check_ip)),
+			('peer' , (TYPE.string, PRESENCE.mandatory , check_ip)),
+			('ttl-security' , (TYPE.int8, PRESENCE.optional , check_nop)),
+			('md5' , (TYPE.string, PRESENCE.optional , check_nop))
 		)))),
 		('api' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
 			('<*>' , (TYPE.list, PRESENCE.mandatory, ['neighbor-changes','send-packets','receive-packets','receive-routes'])),
 		)))),
 		('session' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-			('router-id' , (TYPE.ipv4, PRESENCE.mandatory , None)),
-			('hold-time' , (TYPE.int16, PRESENCE.mandatory , None)),
+			('router-id' , (TYPE.string, PRESENCE.mandatory , check_ipv4)),
+			('hold-time' , (TYPE.int16, PRESENCE.mandatory , check_positive)),
 			('asn' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-				('local' , (TYPE.int32, PRESENCE.mandatory , None)),
-				('peer' , (TYPE.int32, PRESENCE.mandatory , None)),
+				('local' , (TYPE.int32, PRESENCE.mandatory , check_asn)),
+				('peer' , (TYPE.int32, PRESENCE.mandatory , check_asn)),
 			)))),
 			('capability' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
 				('family' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
@@ -126,11 +140,11 @@ definition = (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
 					('inet6' , (TYPE.list, PRESENCE.optional, ['unicast','flow'])),
 					('alias' , (TYPE.string, PRESENCE.optional, ['all','minimal'])),
 				)))),
-				('asn4' , (TYPE.boolean, PRESENCE.optional , None)),
-				('route-refresh' , (TYPE.boolean, PRESENCE.optional , None)),
-				('graceful-restart' , (TYPE.boolean, PRESENCE.optional , None)),
-				('multi-session' , (TYPE.boolean, PRESENCE.optional , None)),
-				('add-path' , (TYPE.boolean, PRESENCE.optional , None)),
+				('asn4' , (TYPE.boolean, PRESENCE.optional , check_nop)),
+				('route-refresh' , (TYPE.boolean, PRESENCE.optional , check_nop)),
+				('graceful-restart' , (TYPE.boolean, PRESENCE.optional , check_nop)),
+				('multi-session' , (TYPE.boolean, PRESENCE.optional , check_nop)),
+				('add-path' , (TYPE.boolean, PRESENCE.optional , check_nop)),
 			))))
 		)))),
 		('announce' , (TYPE.references, PRESENCE.optional,'attributes.updates.prefix.*'))
@@ -138,14 +152,31 @@ definition = (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
 	('api' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
 		('<*>' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
 			('encoder' , (TYPE.string, PRESENCE.optional, ['json','text'])),
-			('program' , (TYPE.string, PRESENCE.mandatory, '<*>')),
+			('program' , (TYPE.string, PRESENCE.mandatory, check_nop)),
 		)))),
 	)))),
 	('attributes' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
+		('<*>' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
+			('next-hop', (TYPE.string, PRESENCE.optional, check_ipv4)),
+			('origin' , (TYPE.string, PRESENCE.optional, ['igp','egp','incomplete'])),
+			('as-path' , (TYPE.list, PRESENCE.optional, check_aspath)),
+			('local-preference', (TYPE.int32, PRESENCE.optional, check_positive)),
+			('med', (TYPE.int32, PRESENCE.optional, check_positive)),
+			('aggregator' , (TYPE.string , PRESENCE.optional, check_ipv4)),
+			('aggregator-id' , (TYPE.string , PRESENCE.optional, check_ipv4)),
+			('atomic-aggregate' , (TYPE.boolean , PRESENCE.optional, check_nop)),
+			('community' , (TYPE.list , PRESENCE.optional, check_community)),
+#			'cluster-list'
+#			'extended-community'
+#			more ?
+		)))),
 	)))),
 	('flow' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
 		('filtering-condition' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
 			('<*>' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
+				('source' , (TYPE.list, PRESENCE.optional, check_ipv4)),
+				('destination' , (TYPE.list, PRESENCE.optional, check_ipv4)),
+				('port' , (TYPE.list, PRESENCE.optional, check_flow_port)),
 			)))),
 		)))),
 		('filtering-action' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
@@ -157,33 +188,6 @@ definition = (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
 	))))
 )))
 
-# 	'attributes': {
-# 		'normal-ebgp-attributes': {
-# 			'origin': 'igp',
-# 			'as-path': [ 3356, 1239, 38040, 9737 ],
-# 			'local-preference': 500,
-# 			'aggregator': '10.0.0.1',
-# 			'atomic-aggregate': false,
-# 			'originator-id': '10.0.0.1',
-# 			'med': 10,
-# 			'community': [[3356,2], [3356,22], [3356,86], [3356,500], [3356,666], [3356,2064]],
-# 			'cluster-list': [],
-# 			'extended-community': []
-# 		},
-# 		'simple-attributes': {
-# 			'next-hop': '212.73.207.153',
-# 			'origin': 'igp',
-# 			'as-path': [ 3356, 1239, 38040, 9737 ],
-# 			'local-preference': 500,
-# 			'aggregator': '10.0.0.1',
-# 			'atomic-aggregate': false,
-# 			'originator-id': '10.0.0.1',
-# 			'med': 10,
-# 			'community': [[3356,2], [3356,22], [3356,86], [3356,500], [3356,666], [3356,2064]],
-# 			'cluster-list': [],
-# 			'extended-community': []
-# 		}
-# 	},
 # 	'filtering-condition': {
 # 		'simple-ddos': {
 # 			'source': '10.0.0.1/32',
@@ -271,10 +275,10 @@ from exabgp.configuration.loader import read
 json = read('/Users/thomas/source/hg/exabgp/tip/QA/configuration/first.exa')
 
 def validate (root,json,definition,location=[]):
-	kind,presence,compare = definition
+	kind,presence,valid = definition
 
 	if kind == TYPE.error:
-		print compare, ' '.join(location)
+		print valid, ' '.join(location)
 		return False
 
 	# ignore missing optional elements
@@ -282,9 +286,13 @@ def validate (root,json,definition,location=[]):
 		print '/'.join(location), 'not present'
 		return presence == PRESENCE.optional
 
+	# check that the value of the right type
+	if not CHECK_TYPE[kind](json):
+		return False
+
 	# for dictionary check all the elements inside
 	if kind == TYPE.dictionary:
-		keys = compare.keys()
+		keys = valid.keys()
 		wildcard = True
 
 		while keys:
@@ -293,7 +301,7 @@ def validate (root,json,definition,location=[]):
 				print "skipping", key
 				continue
 
-			if key in ['announce','attributes']:
+			if key in ['announce','flow']:
 				print "skipping", key
 				continue
 
@@ -306,17 +314,43 @@ def validate (root,json,definition,location=[]):
 				wildcard = False
 				continue
 
-			subtest = compare.get(key,compare.get('<*>',(TYPE.error,None,'problem validating configuration')))
+			print 'key',key
+			subtest = valid.get(key,valid.get('<*>',(TYPE.error,None,'problem validating configuration')))
 			if not validate(root,json.get(key,None),subtest,location + [key]):
 				return False
 		return True
 
-	# check that the value is well in the list
-	if not CHECK[kind](json,definition[2],location):
-		import pdb; pdb.set_trace()
-		return False
+	if kind == TYPE.list:
+		check = definition[2]
+		# This is a function
+		if hasattr(check, '__call__'):
+			for data in json:
+				if not check(data):
+					return False
+			return True
+		# This is a list of valid option
+		elif type(valid) == type([]):
+			for data in json:
+				if not data in valid:
+					return False
+			return True
+		# no idea what the data is - so something is wrong with the program
+		else:
+			return False
 
-	return True
+	else:
+		check = definition[2]
+		# check that the value of the data
+		if hasattr(check, '__call__'):
+			if not check(json):
+				return False
+			return True
+		elif type(valid) == type([]):
+			if not json in valid:
+				return False
+			return True
+		else:
+			return False
 
 print validate(json,json,definition)
 
