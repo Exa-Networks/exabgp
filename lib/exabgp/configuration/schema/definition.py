5031524ffdac063f3b8d0cb8459609fe6d145ed0
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2013-03-22 12:46:21 +0000

    moving forward

--- lib/exabgp/configuration/schema/definition.py
+++ lib/exabgp/configuration/schema/definition.py
@@ -6,10 +6,12 @@ Created by Thomas Mangin on 2013-03-18.
 Copyright (c) 2009-2013 Exa Networks. All rights reserved.
 '''
 
+DEBUG = True
+
 class Enumeration (object):
 	def __init__(self, *names):
 		for number, name in enumerate(names):
-			setattr(self, name, number)
+			setattr(self, name, pow(2,number))
 
 	def text (self,number):
 		for name in dir(self):
@@ -18,54 +20,58 @@ class Enumeration (object):
 
 
 TYPE = Enumeration (
-	'error',
-	'boolean',
-	'int8',
-	'int16',
-	'int32',
-	'string',
-	'list',
-	'dictionary',
-	'ip',
-	'ipv4',
-	'ipv6',
+	'error',       #  0
+	'boolean',     #  1
+	'integer',     #  2
+	'string',      #  4
+	'list',        #  8
+	'dictionary',  # 16
 )
 
 PRESENCE = Enumeration(
-	'optional',
-	'mandatory'
+	'optional',   # 0
+	'mandatory',  # 1
 )
 
 # TYPE CHECK
 def check_boolean (data):
 	return type(data) == type(True)
-def check_int8 (data):
-	return type(data) == type(0) and data < pow(2,8)
-def check_int16 (data):
-	return type(data) == type(0) and data < pow(2,16)
-def check_int32 (data):
-	return type(data) == type(0) and data < pow(2,32)
+def check_integer (data):
+	return type(data) == type(0)
 def check_string (data):
-	return type(data) == type(u'')
+	return type(data) == type(u'') or type(data) == type('')
 def check_list (data):
 	return type(data) == type([])
 def check_dict (data):
 	return type(data) == type({})
 
-
 CHECK_TYPE = {
 	TYPE.boolean : check_boolean,
-	TYPE.int8 : check_int8,
-	TYPE.int16 : check_int16,
-	TYPE.int32 : check_int32,
+	TYPE.integer : check_integer,
 	TYPE.string : check_string,
 	TYPE.list : check_list,
 	TYPE.dictionary : check_dict,
 }
 
+def check_type (kind,data):
+	for t in CHECK_TYPE:
+		if kind & t:
+			if CHECK_TYPE[t](data):
+				return True
+	return False
+
 # DATA CHECK
 def check_nop (data):
 	return True
+
+def check_uint8 (data):
+	return data >= 0 and data < pow(2,8)
+def check_uint16 (data):
+	return data >= 0 and data < pow(2,16)
+def check_uint32 (data):
+	return data >= 0 and data < pow(2,32)
+def check_float (data):
+	return data >=0 and data < 3.4 * pow(10,38)  # approximation of max from wikipedia
 
 def check_ip (data,):
 	return check_ipv4(data) or check_ipv6(data)
@@ -90,8 +96,6 @@ def check_ipv4_range (data):
 		return False
 	return True
 
-def check_positive (data):
-	return data >= 0
 def check_asn (data):
 	return data >= 1
 
@@ -100,10 +104,18 @@ def check_aspath (data):
 	return type(data) == type(0) and data < pow(2,32)
 def check_community (data):
 	return type(data) == type([]) and len(data) == 2 and type(data[0]) == type(0) and type(data[1]) == type(0)
+def check_dscp (data):
+	return check_integer(data) and check_uint8(data)
 
+# FLOW DATA CHECK
+def check_flow_ipv4_range (data):
+	return True
 def check_flow_port (data):
 	return True
 def check_flow_length (data):
+	return True
+
+def check_redirect (data):
 	return True
 
 
@@ -133,128 +145,138 @@ class OrderedDict (dict):
 
 
 attributes = OrderedDict((
-	('next-hop', (TYPE.string, PRESENCE.optional, check_ipv4)),
-	('origin' , (TYPE.string, PRESENCE.optional, ['igp','egp','incomplete'])),
-	('as-path' , (TYPE.list, PRESENCE.optional, check_aspath)),
-	('local-preference', (TYPE.int32, PRESENCE.optional, check_positive)),
-	('med', (TYPE.int32, PRESENCE.optional, check_positive)),
-	('aggregator' , (TYPE.string , PRESENCE.optional, check_ipv4)),
-	('aggregator-id' , (TYPE.string , PRESENCE.optional, check_ipv4)),
-	('atomic-aggregate' , (TYPE.boolean , PRESENCE.optional, check_nop)),
-	('community' , (TYPE.list , PRESENCE.optional, check_community)),
+	('next-hop', (TYPE.string, PRESENCE.optional, '', check_ipv4)),
+	('origin' , (TYPE.string, PRESENCE.optional, '', ['igp','egp','incomplete'])),
+	('as-path' , (TYPE.list, PRESENCE.optional, '', check_aspath)),
+	('local-preference', (TYPE.integer, PRESENCE.optional, '', check_uint32)),
+	('med', (TYPE.integer, PRESENCE.optional, '', check_uint32)),
+	('aggregator' , (TYPE.string , PRESENCE.optional, '', check_ipv4)),
+	('aggregator-id' , (TYPE.string , PRESENCE.optional, '', check_ipv4)),
+	('atomic-aggregate' , (TYPE.boolean , PRESENCE.optional, '', check_nop)),
+	('community' , (TYPE.list , PRESENCE.optional, '', check_community)),
 	# 'cluster-list'
 	# 'extended-community'
 	# more ?
 ))
 
-definition = (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-	('exabgp' , (TYPE.int8, PRESENCE.mandatory, [3,4,])),
-	('neighbor' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-		('tcp' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-			('local' , (TYPE.string, PRESENCE.mandatory , check_ip)),
-			('peer' , (TYPE.string, PRESENCE.mandatory , check_ip)),
-			('ttl-security' , (TYPE.int8, PRESENCE.optional , check_nop)),
-			('md5' , (TYPE.string, PRESENCE.optional , check_nop))
+definition = (TYPE.dictionary, PRESENCE.mandatory, '', OrderedDict((
+	('exabgp' , (TYPE.integer, PRESENCE.mandatory, '', [3,4,])),
+	('neighbor' , (TYPE.dictionary, PRESENCE.mandatory, '', OrderedDict((
+		('tcp' , (TYPE.dictionary, PRESENCE.mandatory, '', OrderedDict((
+			('local' , (TYPE.string, PRESENCE.mandatory, '', check_ip)),
+			('peer' , (TYPE.string, PRESENCE.mandatory, '', check_ip)),
+			('ttl-security' , (TYPE.integer, PRESENCE.optional, '', check_uint8)),
+			('md5' , (TYPE.string, PRESENCE.optional, '', check_nop))
 		)))),
-		('api' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-			('<*>' , (TYPE.list, PRESENCE.mandatory, ['neighbor-changes','send-packets','receive-packets','receive-routes'])),
+		('api' , (TYPE.dictionary, PRESENCE.optional, 'api', OrderedDict((
+			('<*>' , (TYPE.list, PRESENCE.mandatory, '', ['neighbor-changes','send-packets','receive-packets','receive-routes'])),
 		)))),
-		('session' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-			('router-id' , (TYPE.string, PRESENCE.mandatory , check_ipv4)),
-			('hold-time' , (TYPE.int16, PRESENCE.mandatory , check_positive)),
-			('asn' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-				('local' , (TYPE.int32, PRESENCE.mandatory , check_asn)),
-				('peer' , (TYPE.int32, PRESENCE.mandatory , check_asn)),
+		('session' , (TYPE.dictionary, PRESENCE.mandatory, '', OrderedDict((
+			('router-id' , (TYPE.string, PRESENCE.mandatory, '', check_ipv4)),
+			('hold-time' , (TYPE.integer, PRESENCE.mandatory, '', check_uint16)),
+			('asn' , (TYPE.dictionary, PRESENCE.mandatory, '', OrderedDict((
+				('local' , (TYPE.integer, PRESENCE.mandatory, '', check_uint32)),
+				('peer' , (TYPE.integer, PRESENCE.mandatory, '', check_uint32)),
 			)))),
-			('capability' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-				('family' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((
-					('inet'  , (TYPE.list, PRESENCE.optional, ['unicast','multicast','nlri-mpls','mpls-vpn','flow-vpnv4','flow'])),
-					('inet4' , (TYPE.list, PRESENCE.optional, ['unicast','multicast','nlri-mpls','mpls-vpn','flow-vpnv4','flow'])),
-					('inet6' , (TYPE.list, PRESENCE.optional, ['unicast','flow'])),
-					('alias' , (TYPE.string, PRESENCE.optional, ['all','minimal'])),
+			('capability' , (TYPE.dictionary, PRESENCE.mandatory, '', OrderedDict((
+				('family' , (TYPE.dictionary, PRESENCE.mandatory, '', OrderedDict((
+					('inet'  , (TYPE.list, PRESENCE.optional, '', ['unicast','multicast','nlri-mpls','mpls-vpn','flow-vpnv4','flow'])),
+					('inet4' , (TYPE.list, PRESENCE.optional, '', ['unicast','multicast','nlri-mpls','mpls-vpn','flow-vpnv4','flow'])),
+					('inet6' , (TYPE.list, PRESENCE.optional, '', ['unicast','flow'])),
+					('alias' , (TYPE.string, PRESENCE.optional, '', ['all','minimal'])),
 				)))),
-				('asn4' , (TYPE.boolean, PRESENCE.optional , check_nop)),
-				('route-refresh' , (TYPE.boolean, PRESENCE.optional , check_nop)),
-				('graceful-restart' , (TYPE.boolean, PRESENCE.optional , check_nop)),
-				('multi-session' , (TYPE.boolean, PRESENCE.optional , check_nop)),
-				('add-path' , (TYPE.boolean, PRESENCE.optional , check_nop)),
+				('asn4' , (TYPE.boolean, PRESENCE.optional, '', check_nop)),
+				('route-refresh' , (TYPE.boolean, PRESENCE.optional, '', check_nop)),
+				('graceful-restart' , (TYPE.boolean, PRESENCE.optional, '', check_nop)),
+				('multi-session' , (TYPE.boolean, PRESENCE.optional, '', check_nop)),
+				('add-path' , (TYPE.boolean, PRESENCE.optional, '', check_nop)),
 			))))
 		)))),
-		('announce' , (TYPE.list, PRESENCE.optional,'attributes.updates.prefix.*'))
+		('announce' , (TYPE.list, PRESENCE.optional, 'updates.prefix', check_string)),
 	)))),
-	('api' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-		('<*>' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-			('encoder' , (TYPE.string, PRESENCE.optional, ['json','text'])),
-			('program' , (TYPE.string, PRESENCE.mandatory, check_nop)),
+	('api' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+		('<*>' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+			('encoder' , (TYPE.string, PRESENCE.optional, '', ['json','text'])),
+			('program' , (TYPE.string, PRESENCE.mandatory, '', check_nop)),
 		)))),
 	)))),
-	('attributes' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-		('<*>' , (TYPE.dictionary, PRESENCE.optional, attributes)),
+	('attributes' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+		('<*>' , (TYPE.dictionary, PRESENCE.optional, '', attributes)),
 	)))),
-	('flow' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-		('filtering-condition' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-			('<*>' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-				('source' , (TYPE.list, PRESENCE.optional, check_ipv4_range)),
-				('destination' , (TYPE.list, PRESENCE.optional, check_ipv4_range)),
-				('port' , (TYPE.list, PRESENCE.optional, check_flow_port)),
-				('source-port' , (TYPE.list, PRESENCE.optional, check_flow_port)),
-				('destination-port' , (TYPE.list, PRESENCE.optional, check_flow_port)),
-				('protocol' , (TYPE.list, PRESENCE.optional, ['udp','tcp'])),  # and value of protocols ...
-				('packet-length' , (TYPE.list, PRESENCE.optional, check_flow_length)),
-				('packet-fragment' , (TYPE.list, PRESENCE.optional, ['first-fragment', 'last-fragment', 'not-a-fragment'])),  # TODO : missing fragment types
-				('icmp-type' , (TYPE.list, PRESENCE.optional, ['unreachable', 'echo-request', 'echo-reply'])),  # TODO : missing type
-				('icmp-code' , (TYPE.list, PRESENCE.optional, ['host-unreachable', 'network-unreachable'])),  # TODO : missing  code
-				('tcp-flags' , (TYPE.list, PRESENCE.optional, ['urgent', 'rst'])),  # TODO : missing flags
-				('dscp' , (TYPE.list, PRESENCE.optional, check_positive)),
+	('flow' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+		('filtering-condition' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+			('<*>' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+				('source' , (TYPE.list|TYPE.string, PRESENCE.optional, '', check_flow_ipv4_range)),
+				('destination' , (TYPE.list|TYPE.string, PRESENCE.optional, '', check_flow_ipv4_range)),
+				('port' , (TYPE.list, PRESENCE.optional, '', check_flow_port)),
+				('source-port' , (TYPE.list, PRESENCE.optional, '', check_flow_port)),
+				('destination-port' , (TYPE.list, PRESENCE.optional, '', check_flow_port)),
+				('protocol' , (TYPE.list|TYPE.string, PRESENCE.optional, '', ['udp','tcp'])),  # and value of protocols ...
+				('packet-length' , (TYPE.list, PRESENCE.optional, '', check_flow_length)),
+				('packet-fragment' , (TYPE.list|TYPE.string, PRESENCE.optional, '', ['first-fragment', 'last-fragment', 'not-a-fragment'])),  # TODO : missing fragment types
+				('icmp-type' , (TYPE.list|TYPE.string, PRESENCE.optional, '', ['unreachable', 'echo-request', 'echo-reply'])),  # TODO : missing type
+				('icmp-code' , (TYPE.list|TYPE.string, PRESENCE.optional, '', ['host-unreachable', 'network-unreachable'])),  # TODO : missing  code
+				('tcp-flags' , (TYPE.list|TYPE.string, PRESENCE.optional, '', ['urgent', 'rst'])),  # TODO : missing flags
+				('dscp' , (TYPE.list|TYPE.integer, PRESENCE.optional, '', check_dscp)),
 				# MISSING SOME MORE ?
 			)))),
 		)))),
-		('filtering-action' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-			('<*>' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-				('rate-limit' , (TYPE.int16, PRESENCE.optional, check_positive)),
-				('discard' , (TYPE.boolean, PRESENCE.optional, check_nop)),
-				('redirect' , (TYPE.list, PRESENCE.optional, check_nop)),  # TODO : check
-				('community' , (TYPE.list , PRESENCE.optional, check_community)),
+		('filtering-action' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+			('<*>' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+				('rate-limit' , (TYPE.integer, PRESENCE.optional, '', check_float)),
+				('discard' , (TYPE.boolean, PRESENCE.optional, '', check_nop)),
+				('redirect' , (TYPE.list, PRESENCE.optional, '', check_redirect)),
+				('community' , (TYPE.list , PRESENCE.optional, '', check_community)),
 			)))),
 		)))),
 	)))),
-	('updates' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-		('prefix' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-			('<*>' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((  # name of route
-				('<*>' , (TYPE.dictionary, PRESENCE.mandatory, OrderedDict((  # name of attributes referenced
-					('<*>' , (TYPE.dictionary, PRESENCE.optional, attributes)),  # prefix
+	('updates' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+		('prefix' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+			('<*>' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((  # name of route
+				('<*>' , (TYPE.dictionary, PRESENCE.mandatory, '', OrderedDict((  # name of attributes referenced
+					('<*>' , (TYPE.dictionary, PRESENCE.optional, '', attributes)),  # prefix
 				)))),
 			)))),
 		)))),
-		('flow' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((
-			('<*>' , (TYPE.dictionary, PRESENCE.optional, OrderedDict((  # name of the dos
-				('<*>' , (TYPE.list, PRESENCE.mandatory, 'flow.filtering-action.*')),
+		('flow' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((
+			('<*>' , (TYPE.dictionary, PRESENCE.optional, '', OrderedDict((  # name of the dos
+				('<*>' , (TYPE.string, PRESENCE.mandatory, 'flow.filtering-action', check_nop)),
 			)))),
 		)))),
 	)))),
 )))
 
-# 		'flow': {
-# 			'off-goes-the-ddos': {
-# 				'simple-ddos': 'make-it-slow',
-# 				'port-block': 'drop-it'
-# 			},
-# 			'saved_just_in_case': {
-# 				'complex-attack': 'send-it-elsewhere'
-# 			}
-# 		}
-# 	}
-# }
 
 from exabgp.configuration.loader import read
 
 json = read('/Users/thomas/source/hg/exabgp/tip/QA/configuration/first.exa')
 
+def check_reference (root,references,json):
+	if not references:
+		return True
+
+	ref = references if check_list(references) else [references,]
+	jsn = json if check_list(json) else json.keys() if check_dict(json) else [json,]
+
+	for reference in ref:
+		compare = root
+		for path in reference.split('.'):
+			compare = compare.get(path,{})
+
+		for option in jsn:
+			if option in compare:
+				return True
+	return False
+
 def validate (root,json,definition,location=[]):
-	kind,presence,valid = definition
+	kind,presence,references,valid = definition
+	# kind, the type of data possible
+	# presence, indicate if the data is mandatory or not
+	# reference, if the name is a reference to another key
+	# valid, a subdefinition or the check to run
 
 	if kind == TYPE.error:
-		print valid, ' '.join(location)
+		if DEBUG: print 'error reported', valid, ' '.join(location)
 		return False
 
 	# ignore missing optional elements
@@ -263,11 +285,11 @@ def validate (root,json,definition,location=[]):
 		return presence == PRESENCE.optional
 
 	# check that the value of the right type
-	if not CHECK_TYPE[kind](json):
+	if not check_type(kind,json):
 		return False
 
 	# for dictionary check all the elements inside
-	if kind == TYPE.dictionary:
+	if kind & TYPE.dictionary and check_dict(json):
 		keys = valid.keys()
 		wildcard = True
 
@@ -286,48 +308,51 @@ def validate (root,json,definition,location=[]):
 				wildcard = False
 				continue
 
-			print " "*len(location) + key
+			if not check_reference(root,references,json):
+				return False
+
+			if DEBUG: print " "*len(location) + key
 			subtest = valid.get(key,valid.get('<*>',(TYPE.error,None,'problem validating configuration')))
 			if not validate(root,json.get(key,None),subtest,location + [key]):
 				return False
-		return True
 
-	if kind == TYPE.list:
-		check = definition[2]
+	# for list check all the element inside
+	elif kind & TYPE.list and check_list(json):
+		check = valid
 		# This is a function
 		if hasattr(check, '__call__'):
 			for data in json:
 				if not check(data):
 					return False
-			return True
 		# This is a list of valid option
 		elif type(valid) == type([]):
 			for data in json:
 				if not data in valid:
 					return False
-			return True
-		# This is a reference
-		elif type(valid) == type(''):
-			print "reference", valid
-			# TODO: reference checking
-			return True
 		# no idea what the data is - so something is wrong with the program
 		else:
 			return False
 
+	# for non container object check the value
 	else:
-		check = definition[2]
+		check = valid
 		# check that the value of the data
 		if hasattr(check, '__call__'):
 			if not check(json):
 				return False
-			return True
+		# a list of valid option
 		elif type(valid) == type([]):
 			if not json in valid:
 				return False
-			return True
 		else:
 			return False
+
+
+	return check_reference (root,references,json)
+	# if not reference:
+	# 	return False
+	# if not json in reference:
+	# 	return False
 
 print validate(json,json,definition)
 
