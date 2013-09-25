8d16b94418fd6ed917e7368514f588314f8a5d23
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2013-09-25 18:12:19 +0100

    trying to address issue 52

--- setup.py
+++ setup.py
@@ -55,8 +55,8 @@ setup(name='exabgp',
 	scripts=['sbin/exabgp',],
 	download_url='https://github.com/thomas-mangin/exabgp/archive/%s.tar.gz' % version,
 	data_files=[
-		('etc/exabgp',configuration('etc/exabgp')),
-		('usr/lib/systemd/system',configuration('etc/systemd')),
+		('/etc/exabgp',configuration('etc/exabgp')),
+		('/usr/lib/systemd/system',configuration('etc/systemd')),
 	],
 	classifiers=[
 		'Development Status :: 5 - Production/Stable',
