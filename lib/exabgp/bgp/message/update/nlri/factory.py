706f26bb8a31715d4d7e1dc136d4d80007940227
tehnerd <ns.hando@gmail.com>
2014-06-11 17:04:00 +0400

    retabed from <4space> back to <tab>

--- lib/exabgp/bgp/message/update/nlri/factory.py
+++ lib/exabgp/bgp/message/update/nlri/factory.py
@@ -21,160 +21,160 @@ from exabgp.util.od import od
 from exabgp.logger import Logger,LazyFormat
 
 def NLRIFactory (afi,safi,bgp,has_multiple_path,nexthop,action):
-    if safi in (133,134):
-        return _FlowNLRIFactory(afi,safi,nexthop,bgp,action)
-    elif safi in (65,):
-        return _L2vpnNLRIFactory(afi,safi,nexthop,bgp,action)
-    else:
-        return _NLRIFactory(afi,safi,bgp,has_multiple_path,nexthop,action)
+	if safi in (133,134):
+		return _FlowNLRIFactory(afi,safi,nexthop,bgp,action)
+	elif safi in (65,):
+		return _L2vpnNLRIFactory(afi,safi,nexthop,bgp,action)
+	else:
+		return _NLRIFactory(afi,safi,bgp,has_multiple_path,nexthop,action)
 
 def _nlrifactory (afi,safi,bgp,action):
-    labels = []
-    rd = ''
+	labels = []
+	rd = ''
 
-    mask = ord(bgp[0])
-    bgp = bgp[1:]
+	mask = ord(bgp[0])
+	bgp = bgp[1:]
 
-    if SAFI(safi).has_label():
-        while bgp and mask >= 8:
-            label = int(unpack('!L',chr(0) + bgp[:3])[0])
-            bgp = bgp[3:]
-            mask -= 24  # 3 bytes
-            # The last 4 bits are the bottom of Stack
-            # The last bit is set for the last label
-            labels.append(label>>4)
-            # This is a route withdrawal
-            if label == 0x800000 and action == IN.withdrawn:
-                break
-            # This is a next-hop
-            if label == 0x000000:
-                break
-            if label & 1:
-                break
+	if SAFI(safi).has_label():
+		while bgp and mask >= 8:
+			label = int(unpack('!L',chr(0) + bgp[:3])[0])
+			bgp = bgp[3:]
+			mask -= 24	# 3 bytes
+			# The last 4 bits are the bottom of Stack
+			# The last bit is set for the last label
+			labels.append(label>>4)
+			# This is a route withdrawal
+			if label == 0x800000 and action == IN.withdrawn:
+				break
+			# This is a next-hop
+			if label == 0x000000:
+				break
+			if label & 1:
+				break
 
-    if SAFI(safi).has_rd():
-        mask -= 8*8  # the 8 bytes of the route distinguisher
-        rd = bgp[:8]
-        bgp = bgp[8:]
+	if SAFI(safi).has_rd():
+		mask -= 8*8  # the 8 bytes of the route distinguisher
+		rd = bgp[:8]
+		bgp = bgp[8:]
 
-    if mask < 0:
-        raise Notify(3,10,'invalid length in NLRI prefix')
+	if mask < 0:
+		raise Notify(3,10,'invalid length in NLRI prefix')
 
-    if not bgp and mask:
-        raise Notify(3,10,'not enough data for the mask provided to decode the NLRI')
+	if not bgp and mask:
+		raise Notify(3,10,'not enough data for the mask provided to decode the NLRI')
 
-    size = mask_to_bytes.get(mask,None)
-    if size is None:
-        raise Notify(3,10,'invalid netmask found when decoding NLRI')
+	size = mask_to_bytes.get(mask,None)
+	if size is None:
+		raise Notify(3,10,'invalid netmask found when decoding NLRI')
 
-    if len(bgp) < size:
-        raise Notify(3,10,'could not decode route with AFI %d sand SAFI %d' % (afi,safi))
+	if len(bgp) < size:
+		raise Notify(3,10,'could not decode route with AFI %d sand SAFI %d' % (afi,safi))
 
-    network,bgp = bgp[:size],bgp[size:]
-    padding = '\0'*(NLRI.length[afi]-size)
-    prefix = network + padding
+	network,bgp = bgp[:size],bgp[size:]
+	padding = '\0'*(NLRI.length[afi]-size)
+	prefix = network + padding
 
-    return labels,rd,mask,size,prefix,bgp
+	return labels,rd,mask,size,prefix,bgp
 
 
 def _L2vpnNLRIFactory(afi,safi,nexthop,bgp,action):
-    logger = Logger()
-    logger.parser(LazyFormat("parsing l2vpn nlri payload ",od,bgp))
-    if nexthop:
-        nexthop = cachedNextHop(nexthop)
-    nlri = L2vpnNLRI(bgp, action, nexthop)
-    return len(bgp), nlri
+	logger = Logger()
+	logger.parser(LazyFormat("parsing l2vpn nlri payload ",od,bgp))
+	if nexthop:
+		nexthop = cachedNextHop(nexthop)
+	nlri = L2vpnNLRI(bgp, action, nexthop)
+	return len(bgp), nlri
 
 
 def _FlowNLRIFactory (afi,safi,nexthop,bgp,action):
-    logger = Logger()
-    logger.parser(LazyFormat("parsing flow nlri payload ",od,bgp))
+	logger = Logger()
+	logger.parser(LazyFormat("parsing flow nlri payload ",od,bgp))
 
-    total = len(bgp)
-    length,bgp = ord(bgp[0]),bgp[1:]
+	total = len(bgp)
+	length,bgp = ord(bgp[0]),bgp[1:]
 
-    if length & 0xF0 == 0xF0:  # bigger than 240
-        extra,bgp = ord(bgp[0]),bgp[1:]
-        length = ((length & 0x0F) << 16) + extra
+	if length & 0xF0 == 0xF0:  # bigger than 240
+		extra,bgp = ord(bgp[0]),bgp[1:]
+		length = ((length & 0x0F) << 16) + extra
 
-    if length > len(bgp):
-        raise Notify(3,10,'invalid length at the start of the the flow')
+	if length > len(bgp):
+		raise Notify(3,10,'invalid length at the start of the the flow')
 
-    bgp = bgp[:length]
-    nlri = FlowNLRI(afi,safi)
-    nlri.action = action
+	bgp = bgp[:length]
+	nlri = FlowNLRI(afi,safi)
+	nlri.action = action
 
-    if nexthop:
-        nlri.nexthop = cachedNextHop(nexthop)
+	if nexthop:
+		nlri.nexthop = cachedNextHop(nexthop)
 
-    if safi == SAFI.flow_vpn:
-        nlri.rd = RouteDistinguisher(bgp[:8])
-        bgp = bgp[8:]
+	if safi == SAFI.flow_vpn:
+		nlri.rd = RouteDistinguisher(bgp[:8])
+		bgp = bgp[8:]
 
-    seen = []
+	seen = []
 
-    while bgp:
-        what,bgp = ord(bgp[0]),bgp[1:]
+	while bgp:
+		what,bgp = ord(bgp[0]),bgp[1:]
 
-        if what not in decode.get(afi,{}):
-            raise Notify(3,10,'unknown flowspec component received for address family %d' % what)
+		if what not in decode.get(afi,{}):
+			raise Notify(3,10,'unknown flowspec component received for address family %d' % what)
 
-        seen.append(what)
-        if sorted(seen) != seen:
-            raise Notify(3,10,'components are not sent in the right order %s' % seen)
+		seen.append(what)
+		if sorted(seen) != seen:
+			raise Notify(3,10,'components are not sent in the right order %s' % seen)
 
-        decoder = decode[afi][what]
-        klass = factory[afi][what]
+		decoder = decode[afi][what]
+		klass = factory[afi][what]
 
-        if decoder == 'prefix':
-            if afi == AFI.ipv4:
-                _,rd,mask,size,prefix,left = _nlrifactory(afi,safi,bgp,action)
-                adding = klass(prefix,mask)
-                if not nlri.add(adding):
-                    raise Notify(3,10,'components are incompatible (two sources, two destinations, mix ipv4/ipv6) %s' % seen)
-                logger.parser(LazyFormat("added flow %s (%s) payload " % (klass.NAME,adding),od,bgp[:-len(left)]))
-                bgp = left
-            else:
-                byte,bgp = bgp[1],bgp[0]+bgp[2:]
-                offset = ord(byte)
-                _,rd,mask,size,prefix,left = _nlrifactory(afi,safi,bgp,action)
-                adding = klass(prefix,mask,offset)
-                if not nlri.add(adding):
-                    raise Notify(3,10,'components are incompatible (two sources, two destinations, mix ipv4/ipv6) %s' % seen)
-                logger.parser(LazyFormat("added flow %s (%s) payload " % (klass.NAME,adding),od,bgp[:-len(left)]))
-                bgp = left
-        else:
-            end = False
-            while not end:
-                byte,bgp = ord(bgp[0]),bgp[1:]
-                end = CommonOperator.eol(byte)
-                operator = CommonOperator.operator(byte)
-                length = CommonOperator.length(byte)
-                value,bgp = bgp[:length],bgp[length:]
-                adding = klass.decoder(value)
-                nlri.add(klass(operator,adding))
-                logger.parser(LazyFormat("added flow %s (%s) operator %d len %d payload " % (klass.NAME,adding,byte,length),od,value))
+		if decoder == 'prefix':
+			if afi == AFI.ipv4:
+				_,rd,mask,size,prefix,left = _nlrifactory(afi,safi,bgp,action)
+				adding = klass(prefix,mask)
+				if not nlri.add(adding):
+					raise Notify(3,10,'components are incompatible (two sources, two destinations, mix ipv4/ipv6) %s' % seen)
+				logger.parser(LazyFormat("added flow %s (%s) payload " % (klass.NAME,adding),od,bgp[:-len(left)]))
+				bgp = left
+			else:
+				byte,bgp = bgp[1],bgp[0]+bgp[2:]
+				offset = ord(byte)
+				_,rd,mask,size,prefix,left = _nlrifactory(afi,safi,bgp,action)
+				adding = klass(prefix,mask,offset)
+				if not nlri.add(adding):
+					raise Notify(3,10,'components are incompatible (two sources, two destinations, mix ipv4/ipv6) %s' % seen)
+				logger.parser(LazyFormat("added flow %s (%s) payload " % (klass.NAME,adding),od,bgp[:-len(left)]))
+				bgp = left
+		else:
+			end = False
+			while not end:
+				byte,bgp = ord(bgp[0]),bgp[1:]
+				end = CommonOperator.eol(byte)
+				operator = CommonOperator.operator(byte)
+				length = CommonOperator.length(byte)
+				value,bgp = bgp[:length],bgp[length:]
+				adding = klass.decoder(value)
+				nlri.add(klass(operator,adding))
+				logger.parser(LazyFormat("added flow %s (%s) operator %d len %d payload " % (klass.NAME,adding,byte,length),od,value))
 
-    return total-len(bgp),nlri
+	return total-len(bgp),nlri
 
 def _NLRIFactory (afi,safi,bgp,has_multiple_path,nexthop,action):
-    if has_multiple_path:
-        path_identifier = bgp[:4]
-        bgp = bgp[4:]
-        length = 4
-    else:
-        path_identifier = ''
-        length = 0
+	if has_multiple_path:
+		path_identifier = bgp[:4]
+		bgp = bgp[4:]
+		length = 4
+	else:
+		path_identifier = ''
+		length = 0
 
-    labels,rd,mask,size,prefix,left = _nlrifactory(afi,safi,bgp,action)
+	labels,rd,mask,size,prefix,left = _nlrifactory(afi,safi,bgp,action)
 
-    nlri = NLRI(afi,safi,prefix,mask,cachedNextHop(nexthop),action)
+	nlri = NLRI(afi,safi,prefix,mask,cachedNextHop(nexthop),action)
 
-    if path_identifier:
-        nlri.path_info = PathInfo(packed=path_identifier)
-    if labels:
-        nlri.labels = Labels(labels)
-    if rd:
-        nlri.rd = RouteDistinguisher(rd)
+	if path_identifier:
+		nlri.path_info = PathInfo(packed=path_identifier)
+	if labels:
+		nlri.labels = Labels(labels)
+	if rd:
+		nlri.rd = RouteDistinguisher(rd)
 
-    return length + len(bgp) - len(left),nlri
+	return length + len(bgp) - len(left),nlri
