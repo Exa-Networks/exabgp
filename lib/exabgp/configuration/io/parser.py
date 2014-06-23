047231c768b12ec63b80942230cdb0cba66cb09d
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2014-06-24 00:37:06 +0100

    new parsing code based on previous json work

---
+++ lib/exabgp/configuration/io/parser.py
@@ -0,0 +1,123 @@
+from exabgp.util import coroutine
+
+class UnexpectedData (Exception):
+	def __init__(self, line, position, token):
+		super(UnexpectedData, self).__init__('Unexpected data at line %d position %d : "%s"' % (line,position,token))
+
+@coroutine.join
+def unescape(s):
+	start = 0
+	while start < len(s):
+		pos = s.find('\\', start)
+		if pos == -1:
+			yield s[start:]
+			break
+		yield s[start:pos]
+		pos += 1
+		esc = s[pos]
+		if esc == 'b':
+			yield '\b'
+		elif esc == 'f':
+			yield '\f'
+		elif esc == 'n':
+			yield '\n'
+		elif esc == 'r':
+			yield '\r'
+		elif esc == 't':
+			yield '\t'
+		elif esc == 'u':
+			yield chr(int(s[pos + 1:pos + 5], 16))
+			pos += 4
+		else:
+			yield esc
+		start = pos + 1
+
+@coroutine.each
+def tokens (stream):
+	spaces = [' ','\t','\r','\n']
+	strings = ['"', "'"]
+	syntax = [',','[',']','{','}',';']
+	comment = ['#',]
+	nb_lines = 0
+	for line in stream:
+		nb_chars = 0
+		quoted = ''
+		word = ''
+		for char in line:
+			if char in comment:
+				if quoted:
+					word += char
+				else:
+					if word:
+						yield nb_lines,nb_chars,char
+						word = ''
+					break
+
+			elif char in syntax:
+				if quoted:
+					word += char
+				else:
+					if word:
+						yield nb_lines,nb_chars,word
+						nb_chars += len(word)
+						word = ''
+					yield nb_lines,nb_chars,char
+				nb_chars += 1
+
+			elif char in spaces:
+				if quoted:
+					word += char
+				elif word:
+					yield nb_lines,nb_chars,word
+					nb_chars += len(word)
+					word = ''
+				nb_chars += 1
+
+			elif char in strings:
+				word += char
+				if quoted == char:
+					quoted = ''
+					yield nb_lines,nb_chars,word
+					nb_chars += len(word) + 1
+					word = ''
+				else:
+					quoted = char
+					nb_chars += 1
+
+			else:
+				word += char
+				nb_chars += 1
+
+def parser (tokeniser):
+	def content(next):
+		try:
+			while True:
+				line,position,token = next()
+
+				if token == '[':
+					l = []
+					for element in iterate_list(next):
+						l.append(element)
+					return l
+				elif token[0] == '"':
+					return unescape(token[1:-1])
+				elif token == 'true':
+					return True
+				elif token == 'false':
+					return False
+				elif token == 'null':
+					return None
+				else:
+					return token
+		except ValueError:
+			raise UnexpectedData(line,position,token)
+		except StopIteration:
+			return ''
+
+	def iterate_list(next):
+		token = content(next)
+		while token != ']':
+			yield token
+			token = content(next)
+
+	return content(tokeniser)
---
+++ lib/exabgp/configuration/io/reader.py
@@ -0,0 +1,133 @@
+# encoding: utf-8
+"""
+reader.py
+
+Created by Thomas Mangin on 2013-03-15.
+Copyright (c) 2009-2013 Exa Networks. All rights reserved.
+"""
+
+class InvalidFormat (Exception):
+	"Raised when the configuration can not be parsed"
+	pass
+
+
+class Format (object):
+	"""
+	Store class used to convert the configuration format to json
+	Every configuration file must start with the line "#syntax: <format>\n"
+	where <format> is one of the class defined within format (simplejson or json)
+	"""
+
+	class exabgp (object):
+		"""
+		raw reader without any modification
+		"""
+		@staticmethod
+		def skip (current):
+			return False
+
+		@staticmethod
+		def read (last,current):
+			return current
+
+
+class Reader (object):
+	"""
+	A file-like object providing a read() method which will convert
+	the configuration in JSON following the format information set at
+	the start of the file with the "#syntax: <format>"
+	"""
+	def __init__ (self,fname):
+		self.file = open(fname,'rb')
+		self.last = ''      # the last line we read from the file
+		self.formated = ''  # the formated data we have already converted
+
+		name = ''.join(self.file.readline().split())
+		if not name.startswith('#syntax:'):
+			name = '#syntax:exabgp'
+			self.file.close()
+			self.file = open(fname,'rb')
+
+		klass = getattr(Format,name[8:],None)
+		if not klass:
+			raise InvalidFormat('unknown configuration format')
+
+		self.format = klass.read
+		self.skip = klass.skip
+
+	def __del__(self):
+		if self.file:
+			self.file.close()
+			self.file = None
+
+	def __enter__ (self):
+		return self
+
+	def __exit__(self, type, value, tb):
+		if self.file:
+			self.file.close()
+			self.file = None
+
+	def read (self,number=0):
+		# we already done the work, just return the small chunks
+		if number and len(self.formated) >= number:
+			returned, self.formated = self.formated[:number], self.formated[number:]
+			return returned
+
+		data = bytearray()
+
+		try:
+			# restore / init the last line seen
+			last = self.last
+
+			# reading up to number bytes or until EOF which will raise StopIteration
+			while not number or len(data) < number:
+				new = self.file.next()
+				if self.skip(new):
+					continue
+				data += self.format(last,new)
+				last = new
+
+			# save the last line seen for the next call
+			self.last = last
+
+			if number:
+				complete = self.formated + bytes(data)
+				returned, self.formated = complete[:number], complete[number:]
+				return returned
+
+			return bytes(data)
+		except StopIteration:
+			# we can come here twice : on EOF and again
+			# to empty self.formated when its len becomes smaller than number
+			complete = self.formated + bytes(data)
+			if number:
+				returned, self.formated = complete[:number], complete[number:]
+				return returned
+			else:
+				self.formated = ''
+				return complete
+
+	def readline (self, limit=-1):
+		returned = bytearray()
+		while limit < 0 or len(returned) < limit:
+			byte = self.read(1)
+			if not byte:
+				break
+			returned += byte
+			if returned.endswith(b'\n'):
+				break
+		return bytes(returned)
+
+	def __iter__ (self):
+		if not self.file:
+			raise ValueError("I/O operation on closed file.")
+		return self
+
+	def next (self):
+		line = self.readline()
+		if not line:
+			raise StopIteration
+		return line
+
+	__next__ = next
