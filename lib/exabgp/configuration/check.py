cd5ecfc421eba57eae7aa88ebd57d1fb0f8d0d4f
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2014-07-08 16:33:35 +0100

    undo test

--- lib/exabgp/configuration/check.py
+++ lib/exabgp/configuration/check.py
@@ -219,7 +219,7 @@ def check_update (neighbor,raw):
 		for number in range(len(update.nlris)):
 			change = Change(update.nlris[number],update.attributes)
 			logger.parser('decoded %s %s %s' % (decoding,change.nlri.action,change.extensive()))
-		logger.parser('update json %s' % JSON('3.4.0').message(Update.ID,p,update,'',''))
+		logger.parser('update json %s' % JSON('3.4.0').update(p,update,'',''))
 
 	return True
 
