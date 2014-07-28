197d231b844dc0cde9d6112896018153adfa72d3
Wouter Miltenburg <w.a.miltenburg@gmail.com>
2014-07-28 13:34:08 +0200

    Variable name changed, but we didn't change the variable in this call.

--- lib/exabgp/reactor/network/connection.py
+++ lib/exabgp/reactor/network/connection.py
@@ -216,7 +216,7 @@ class Connection (object):
 		validator = Message.Length.get(msg,lambda _ : _ >= 19)
 		if not validator(length):
 			# MUST send the faulty msg_length back
-			yield length,0,header,'',NotifyError(1,2,'%s has an invalid message length of %d' %(Message().name(msg),msg_length))
+			yield length,0,header,'',NotifyError(1,2,'%s has an invalid message length of %d' %(Message().name(msg),length))
 			return
 
 		number = length - Message.HEADER_LEN
