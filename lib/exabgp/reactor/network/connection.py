0f77640be386e1c8c04a41fcfe0da2face1af8c7
Wouter Miltenburg <w.a.miltenburg@gmail.com>
2014-08-06 13:15:02 +0200

    fix posisble socket block

--- lib/exabgp/reactor/network/connection.py
+++ lib/exabgp/reactor/network/connection.py
@@ -133,6 +133,8 @@ class Connection (object):
 						self.logger.wire(LazyFormat("%s %-32s RECEIVED " % (self.name(),'%s / %s' % (self.local,self.peer)),od,read))
 						yield data
 						return
+					
+					yield ''
 			except socket.timeout,e:
 				self.close()
 				self.logger.wire("%s %s peer is too slow" % (self.name(),self.peer))
