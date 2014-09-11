27c5712474e61dc42612951ee15ea86a15bd32e4
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2014-09-11 14:07:09 +0100

    fix ipv4/ipv6 bug

--- lib/exabgp/protocol/ip/__init__.py
+++ lib/exabgp/protocol/ip/__init__.py
@@ -151,7 +151,7 @@ class IPv4 (IP):
 	__slots__ = []
 
 	def __init__ (self,ip,packed=None):
-		self.init(ip,packed if packed else socket.inet_pton(socket.AF_INET,ip))
+		self.init(ip,packed if packed else IP.pton(ip))
 
 	def __len__ (self):
 		return 4
