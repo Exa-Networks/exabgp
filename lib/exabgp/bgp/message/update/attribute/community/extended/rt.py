37f7e302acb87d5921e9967e054627a989989f04
Michał Grzędzicki <lazy404@gmail.com>
2014-09-16 13:07:14 +0200

    show L in 32 bit target communities

--- lib/exabgp/bgp/message/update/attribute/community/extended/rt.py
+++ lib/exabgp/bgp/message/update/attribute/community/extended/rt.py
@@ -94,7 +94,7 @@ class RouteTargetASN4Number (RouteTarget):
 		RouteTarget.__init__(self,community if community else pack('!BBLH',self.COMMUNITY_TYPE|0x40 if transitive else self.COMMUNITY_TYPE,0x02,asn,number))
 
 	def __str__ (self):
-		return "target:%d:%s" % (self.asn, self.number)
+		return "target:%dL:%s" % (self.asn, self.number)
 
 	@staticmethod
 	def unpack (data):
