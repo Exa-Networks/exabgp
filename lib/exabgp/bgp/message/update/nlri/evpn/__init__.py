a35b19ed34c24bf4cfa14e15f35917445a752e5b
garci66 <garci66@gmail.com>
2015-03-15 21:15:59 -0700

    Add support for prefix advertisements (RT=5)
    Added support for RT-5 type EVPN NLRI. the class has been derived from
    the work done for the evpn mac class.

--- lib/exabgp/bgp/message/update/nlri/evpn/__init__.py
+++ lib/exabgp/bgp/message/update/nlri/evpn/__init__.py
@@ -11,8 +11,10 @@ from exabgp.bgp.message.update.nlri.evpn.ethernetad import EthernetAD
 from exabgp.bgp.message.update.nlri.evpn.mac import MAC
 from exabgp.bgp.message.update.nlri.evpn.multicast import Multicast
 from exabgp.bgp.message.update.nlri.evpn.segment import EthernetSegment
+from exabgp.bgp.message.update.nlri.evpn.prefix import Prefix
 
 # EVPN.register_evpn(EthernetAD)
 EVPN.register_evpn(MAC)
 EVPN.register_evpn(Multicast)
 # EVPN.register_evpn(EthernetSegment)
+EVPN.register_evpn(Prefix)
