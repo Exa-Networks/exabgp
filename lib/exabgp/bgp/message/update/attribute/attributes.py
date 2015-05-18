871678f881c797ee23dad86d8ec7153c100e7526
Thomas Morin <thomas.morin@orange.com>
2015-05-18 15:43:16 -0700

    sameValuesAs does sorting for Communities only

--- lib/exabgp/bgp/message/update/attribute/attributes.py
+++ lib/exabgp/bgp/message/update/attribute/attributes.py
@@ -16,6 +16,7 @@ from exabgp.bgp.message.update.attribute.generic import GenericAttribute
 from exabgp.bgp.message.update.attribute.origin import Origin
 from exabgp.bgp.message.update.attribute.aspath import ASPath
 from exabgp.bgp.message.update.attribute.localpref import LocalPreference
+from exabgp.bgp.message.update.attribute.community.communities import Communities
 
 from exabgp.bgp.message.notification import Notify
 
@@ -385,10 +386,10 @@ class Attributes (dict):
 				sval = self[key]
 				oval = other[key]
 
-				# In the case where the attribute is, for instance, a list
-				# we want to compare values independently of the order
-				if isinstance(sval, collections.Iterable):
-					if not isinstance(oval, collections.Iterable):
+				# In the case where the attribute is Communities or
+				# extended communities, we want to compare values independently of their order
+				if isinstance(sval, Communities):
+					if not isinstance(oval, Communities):
 						return False
 
 					sval = sorted(sval,sorter)
