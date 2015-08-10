7897721e6b2f4be5b2d3b518a16eb1d4462018fc
Vincent Bernat <vincent@bernat.im>
2015-08-11 00:47:25 +0200

    healthcheck: add ability to drop privileges
    After setting loopback IPs, privileges of healthcheck application can be
    dropped to some specified user. Related to #312.

--- lib/exabgp/application/healthcheck.py
+++ lib/exabgp/application/healthcheck.py
@@ -108,6 +108,10 @@ def parse():
     parser.add_argument("--pid", "-p", metavar="FILE",
                         type=argparse.FileType('w'),
                         help="write PID to the provided file")
+    parser.add_argument("--user", metavar="USER",
+                        help="set user after setting loopback addresses")
+    parser.add_argument("--group", metavar="GROUP",
+                        help="set group after setting loopback addresses")
 
     g = parser.add_argument_group("checking healthiness")
     g.add_argument("--interval", "-i", metavar='N',
@@ -294,6 +298,26 @@ def setup_ips(ips, label):
                 cmd += ["label", "lo:{0}".format(label)]
             subprocess.check_call(
                 cmd, stdout=fnull, stderr=fnull)
+
+
+def drop_privileges(user, group):
+    """Drop privileges to specified user and group"""
+    if group is not None:
+        import grp
+        gid = grp.getgrnam(group).gr_gid
+        logger.debug("Dropping privileges to group {0}/{1}".format(group, gid))
+        try:
+            os.setresgid(gid, gid, gid)
+        except AttributeError:
+            os.setregid(gid, gid)
+    if user is not None:
+        import pwd
+        uid = pwd.getpwnam(user).pw_uid
+        logger.debug("Dropping privileges to user {0}/{1}".format(user, uid))
+        try:
+            os.setresuid(uid, uid, uid)
+        except AttributeError:
+            os.setreuid(uid, uid)
 
 
 def check(cmd, timeout):
@@ -490,6 +514,7 @@ def main():
             sys.exit(1)
         if options.ip_setup:
             setup_ips(options.ips, options.label)
+        drop_privileges(options.user, options.group)
         options.ips = collections.deque(options.ips)
         options.ips.rotate(-options.start_ip)
         options.ips = list(options.ips)
