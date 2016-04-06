c1aedb95cf44d490755b9ac45c6e35bdbf5fa842
Bryan Le Gear <b.legear@gmail.com>
2016-04-06 12:12:55 +0100

    "now" variable isnt an int , dont use %d string is of form : Wed, 06 Apr 2016 11:58:24 / time.strftime('%a, %d %b %Y %H:%M:%S',timestamp)

--- lib/exabgp/logger.py
+++ lib/exabgp/logger.py
@@ -155,7 +155,7 @@ class Logger (object):
 		elif self._where in ['syslog',]:
 			return "%s: %-6d %-13s %s" % (environment.application,self._pid,source,message)
 		elif self._where in ['file',]:
-			return "%d %-6d %-13s %s" % (now,self._pid,source,message)
+			return "%s %-6d %-13s %s" % (now,self._pid,source,message)
 		else:
 			# failsafe
 			return "%s | %-8s | %-6d | %-13s | %s" % (now,level,self._pid,source,message)
