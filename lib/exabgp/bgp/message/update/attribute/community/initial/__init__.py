154e763e909f55849ae801b544ee597ed87a7fe3
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2016-09-04 13:06:03 +0100

    TODO reminder

--- lib/exabgp/bgp/message/update/attribute/community/initial/__init__.py
+++ lib/exabgp/bgp/message/update/attribute/community/initial/__init__.py
@@ -8,3 +8,7 @@ Copyright (c) 2009-2015 Exa Networks. All rights reserved.
 
 from exabgp.bgp.message.update.attribute.community.initial.community import Community
 from exabgp.bgp.message.update.attribute.community.initial.communities import Communities
+
+
+# TODO: we should have the common code for the three kind of community separated
+# TODO: and this is what should be used for inheritance.
