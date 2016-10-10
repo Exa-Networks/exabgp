541f0ee01e42d2f191677add2e7914e43c9effc2
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2016-10-10 15:27:49 +0100

    satisfy Section 2 of draft-ietf-idr-large-community-01

--- lib/exabgp/bgp/message/update/attribute/community/large/communities.py
+++ lib/exabgp/bgp/message/update/attribute/community/large/communities.py
@@ -21,6 +21,9 @@ class LargeCommunities (Communities):
 		while data:
 			if data and len(data) < 12:
 				raise Notify(3,1,'could not decode large community %s' % str([hex(ord(_)) for _ in data]))
-			large_communities.add(LargeCommunity.unpack(data[:12],negotiated))
+			lc = LargeCommunity.unpack(data[:12],negotiated)
 			data = data[12:]
+			if lc in large_communities.communities:
+				continue
+			large_communities.add(lc)
 		return large_communities
