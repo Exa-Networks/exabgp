b89a2904500df87a5250560aa089642ad5a8a424
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2016-10-10 15:02:41 +0100

    space to tab conversion

--- lib/exabgp/bgp/message/update/attribute/community/large/communities.py
+++ lib/exabgp/bgp/message/update/attribute/community/large/communities.py
@@ -13,14 +13,14 @@ from exabgp.bgp.message.notification import Notify
 
 @Attribute.register()
 class LargeCommunities (Communities):
-    ID = Attribute.CODE.LARGE_COMMUNITY
+	ID = Attribute.CODE.LARGE_COMMUNITY
 
-    @staticmethod
-    def unpack (data, negotiated):
-        large_communities = LargeCommunities()
-        while data:
-            if data and len(data) < 12:
-                raise Notify(3,1,'could not decode large community %s' % str([hex(ord(_)) for _ in data]))
-            large_communities.add(LargeCommunity.unpack(data[:12],negotiated))
-            data = data[12:]
-        return large_communities
+	@staticmethod
+	def unpack (data, negotiated):
+		large_communities = LargeCommunities()
+		while data:
+			if data and len(data) < 12:
+				raise Notify(3,1,'could not decode large community %s' % str([hex(ord(_)) for _ in data]))
+			large_communities.add(LargeCommunity.unpack(data[:12],negotiated))
+			data = data[12:]
+		return large_communities
