6c5116d78c5d4b87e0f83bf61a0913d32677dd61
Thomas Morin <thomas.morin@orange.com>
2017-03-06 21:26:53 +0100

    python3: fix int/str mix in RTRecord

--- lib/exabgp/bgp/message/update/attribute/community/extended/rt_record.py
+++ lib/exabgp/bgp/message/update/attribute/community/extended/rt_record.py
@@ -1,7 +1,7 @@
 from exabgp.bgp.message.update.attribute.community.extended import ExtendedCommunity
 from exabgp.bgp.message.update.attribute.community.extended import rt
 
-# draft-fm-bess-service-chaining
+# draft-ietf-bess-service-chaining
 
 
 class RTRecord(rt.RouteTarget):
@@ -11,8 +11,7 @@ class RTRecord(rt.RouteTarget):
 	@classmethod
 	def from_rt(cls, route_target):
 		packed = route_target.pack()
-		return cls.unpack(packed[0] + chr(cls.COMMUNITY_SUBTYPE) + packed[2:])
-
+		return cls.unpack(chr(packed[0]) + chr(cls.COMMUNITY_SUBTYPE) + str(packed[2:]))
 
 @ExtendedCommunity.register
 class RTRecordASN2Number(RTRecord, rt.RouteTargetASN2Number):
