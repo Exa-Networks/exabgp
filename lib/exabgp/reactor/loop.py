63adead886b6e5511ee0cca34283d41e5bd43348
Dhammika Pathirana <dhammika@gmail.com>
2017-08-23 14:31:01 -0700

    Fix api poller #709
    Signed-off-by: Dhammika Pathirana <dhammika@gmail.com>

--- lib/exabgp/reactor/loop.py
+++ lib/exabgp/reactor/loop.py
@@ -64,8 +64,8 @@ class Reactor (object):
 		self.signal.received = Signal.SHUTDOWN
 		self.logger.critical(reason,'reactor')
 
-	def _api_ready (self,sockets):
-		sleeptime = 0 if self.async.ready() else self.max_loop_time / 100
+	def _api_ready (self,sockets,dontsleep=False):
+		sleeptime = 0 if dontsleep or self.async.ready() else self.max_loop_time / 100
 		fds = self.processes.fds()
 		ios = fds + sockets
 		try:
@@ -244,7 +244,7 @@ class Reactor (object):
 
 				self.async.run()
 
-				for io in self._api_ready(list(workers)):
+				for io in self._api_ready(list(workers),len(peers)):
 					peers.add(workers[io])
 					del workers[io]
 
