6aeb5690055662901c3d97e28ec2f734a5dde90a
Thomas Morin <thomas.morin@orange.com>
2017-11-16 16:54:32 +0100

    EVPN support for VXLAN
    In EVPN/VXLAN [1] a VXLAN VNI is stored inside the 3-byte fields used
    for MPLS labels in RFC7432. In this case the 4-bit shifting done for
    MPLS labels does not apply anymore. This applies to label fields in NLRI
    routes and to the label field in the PMSI tunnel attribute.
    
    This change updates corresponding classes to:
    - expose both the MPLS 4-bit shifted value, and the raw value, in
      parsed routes
    - allow passing a 'raw_label' value in routes to be encoded,
      when provided this value will be used for packing,
      instead of using the 'label' after 4-bit shifting
    - update string representation to show both the 4-bit shifted label
      and the raw label
    
    [1] https://tools.ietf.org/html/draft-ietf-bess-evpn-overlay

--- lib/exabgp/bgp/message/update/nlri/qualifier/labels.py
+++ lib/exabgp/bgp/message/update/nlri/qualifier/labels.py
@@ -20,18 +20,27 @@ from exabgp.util import concat_bytes_i
 class Labels (object):
 	MAX = pow(2,20)-1
 
-	__slots__ = ['labels','packed','_len']
+	__slots__ = ['labels','packed','_len','raw_labels']
 
-	def __init__ (self, labels, bos=True):
+	def __init__ (self, labels, bos=True, raw_labels=None):
 		self.labels = labels
+		self.raw_labels = raw_labels
 		packed = []
-		for label in labels:
-			# shift to 20 bits of the label to be at the top of three bytes and then truncate.
-			packed.append(pack('!L',label << 4)[1:])
-		# Mark the bottom of stack with the bit
-		if packed and bos:
-			packed.pop()
-			packed.append(pack('!L',(label << 4) | 1)[1:])
+		if raw_labels:
+			for label in raw_labels:
+				packed.append(pack('!L',label)[1:])
+			# fill self.labels as well, not for packing, but to allow
+			# consistent string representations
+			if not self.labels:
+				self.labels = [x >> 4 for x in self.raw_labels]
+		else:
+			for label in labels:
+				# shift to 20 bits of the label to be at the top of three bytes and then truncate.
+				packed.append(pack('!L',label << 4)[1:])
+			# Mark the bottom of stack with the bit
+			if packed and bos:
+				packed.pop()
+				packed.append(pack('!L',(label << 4) | 1)[1:])
 		self.packed = concat_bytes_i(packed)
 		self._len = len(self.packed)
 
@@ -60,37 +69,39 @@ class Labels (object):
 		return self._len
 
 	def json (self):
-		if self._len >= 1:
-			return '"label": [ %s ]' % ', '.join([str(_) for _ in self.labels])
+		if len(self.labels) >= 1:
+			return '"label": [ %s ]' % ', '.join(["[%d, %d]" % (l,r) for (l,r) in zip(self.labels, self.raw_labels)])
 		else:
 			return ''
 
 	def __str__ (self):
-		if self._len > 1:
-			return ' label [ %s ]' % ' '.join([str(_) for _ in self.labels])
-		elif self._len == 1:
-			return ' label %s' % self.labels[0]
+		if len(self.labels) > 1:
+			return ' label [ %s ]' % ' '.join(["%d (%d)" % (l,r) for (l,r) in zip(self.labels, self.raw_labels)])
+		elif len(self.labels) == 1:
+			return ' label %s (%d)' % (self.labels[0], self.raw_labels[0])
 		else:
 			return ''
 
 	def __repr__(self):
-		if self._len > 1:
-			return '[ %s ]' % ','.join([str(_) for _ in self.labels])
-		elif self._len == 1:
-			return '%d' % self.labels[0]
+		if len(self.labels) > 1:
+			return '[ %s ]' % ','.join(["%d (%d)" % (l,r) for (l,r) in zip(self.labels, self.raw_labels)])
+		elif len(self.labels) == 1:
+			return '%d (%d)' % (self.labels[0], self.raw_labels[0])
 		else:
 			return '[ ]'
 
 	@classmethod
 	def unpack (cls, data):
 		labels = []
+		raw_labels = []
 		while len(data):
 			label = unpack('!L',character(0)+data[:3])[0]
 			data = data[3:]
 			labels.append(label >> 4)
+			raw_labels.append(label)
 			if label & 0x001:
 				break
-		return cls(labels)
+		return cls(labels, raw_labels=raw_labels)
 
 
 Labels.NOLABEL = Labels([])
