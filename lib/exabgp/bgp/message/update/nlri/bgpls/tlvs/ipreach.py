6655f996aac756e8c6c3e2072729094995ee1a3a
Havard Eidnes <he@uninett.no>
2018-06-29 11:52:23 +0200

    Perhaps now we have zero diff here?

--- lib/exabgp/bgp/message/update/nlri/bgpls/tlvs/ipreach.py
+++ lib/exabgp/bgp/message/update/nlri/bgpls/tlvs/ipreach.py
@@ -60,7 +60,7 @@ class IpReach(object):
 		# fill the rest of the octets with 0 to construct
 		# a 4 octet IP prefix
 		prefix_list = prefix_list + ["0"]*(4 - len(prefix_list))
-		prefix = '.'.join(prefix_list)
+		prefix = '.'.join(prefix_list) 
 		return cls(prefix=prefix, plength=plength)
 
 	def json (self, compact=None):
