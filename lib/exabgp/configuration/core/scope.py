36f1bebe79c8058b1783b72fd25eb7d72a1ad006
Thomas Mangin <thomas.mangin@exa.net.uk>
2019-05-12 09:34:58 +0100

    better error message for #872

--- lib/exabgp/configuration/core/scope.py
+++ lib/exabgp/configuration/core/scope.py
@@ -161,7 +161,7 @@ class Scope (Error):
 			elif isinstance(source[key],IP):
 				destination[key] = value
 			else:
-				self.throw('can not recursively copy this type of data %s' % type(source[key]))
+				self.throw('can not copy "%s" (as it is of type %s) and it exists in both the source and destination' % (key,type(source[key])))
 
 	def get (self, name='', default=None):
 		if name:
