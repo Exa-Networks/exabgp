f4bd573ff549e6dd977a1684014c30af119f1ff0
Thomas Mangin <thomas.mangin@exa.net.uk>
2020-01-09 22:54:26 +0000

    Sorry Håvard Eidnes, your name makes python2 crash

--- lib/exabgp/bgp/message/update/nlri/bgpls/prefixv6.py
+++ lib/exabgp/bgp/message/update/nlri/bgpls/prefixv6.py
@@ -1,6 +1,6 @@
 """
 prefixv6.py
-Created by Tinus Flagstad & Håvard Eidnes on 2018-07-02.
+Created by Tinus Flagstad & Havard Eidnes on 2018-07-02.
 Copyright (c) 2009-2018 Exa Networks. All rights reserved.
 License: 3-clause BSD. (See the COPYRIGHT file)
 """
