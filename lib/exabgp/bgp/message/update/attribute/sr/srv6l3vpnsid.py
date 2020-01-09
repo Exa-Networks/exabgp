3b885425a1337c2dc0d525f39986f4a2b1a629bc
Hiroki Shirokura <slank.dev@gmail.com>
2020-01-09 14:21:08 +0000

    add support prefix-sid type-5 srv6 l3vpn service
    This commit makes exabgp to support prefix-sid sub-type-5
    that is defined as L3vpn-focused vpn-sid by following draft.
    This prefix-sid is already supported cisco's NX-OS.
    
    - https://tools.ietf.org/html/draft-dawra-idr-srv6-vpn-05
    
    Signed-off-by: Hiroki Shirokura <slank.dev@gmail.com>

---
+++ lib/exabgp/bgp/message/update/attribute/sr/srv6l3vpnsid.py
@@ -0,0 +1,63 @@
+# encoding: utf-8
+"""
+sr/srv6l3vpnsid.py
+
+Created by Hiroki Shirokura 2020-01-09
+Copyright (c) 2020 Hiroki Shirokura . All rights reserved.
+"""
+from struct import pack
+
+from exabgp.util import concat_bytes
+from exabgp.protocol.ip import IP
+
+from exabgp.bgp.message.notification import Notify
+from exabgp.bgp.message.update.attribute.sr.prefixsid import PrefixSid
+
+# 0                   1                   2                   3
+# 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# |       Type    |             Length            |   RESERVED    |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# |                                                               |
+# |                      L3VPN SID (16 octets)                    |
+# |                                                               |
+# |                                                               |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# | 　SID Flags   |       Endpoint Behavior       |   RESERVED    |
+# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# 3.2.  SRv6 L3 Service
+
+
+@PrefixSid.register()
+class Srv6L3vpnSid(object):
+	TLV = 5
+	LENGTH = 21
+
+	def __init__ (self,l3vpnsid,packed=None):
+		self.l3vpnsid = l3vpnsid
+		self.packed = self.pack()
+
+	def __repr__ (self):
+		return "srv6-l3vpn-sid %s" % (self.l3vpnsid)
+
+	def pack (self):
+		return concat_bytes(
+			pack('!B', self.TLV),
+			pack('!H', self.LENGTH),
+			pack('!B', 0),
+			IP.pton(self.l3vpnsid),
+			pack('!B', 0),
+			pack('!H', 0xffff),
+			pack('!B', 0),
+		)
+
+	@classmethod
+	def unpack (cls,data,length):
+		l3vpnsid = -1
+		if cls.LENGTH != length:
+			raise Notify(3,5, "Invalid TLV size. Should be {0} but {1} received".format(cls.LENGTH, length))
+		l3vpnsid = IP.unpack(data[1:17])
+		return cls(l3vpnsid=str(l3vpnsid),packed=data)
+
+	def json (self, compact=None):
+		return '"srv6-l3vpn-sid": "%s"' % (self.l3vpnsid)
