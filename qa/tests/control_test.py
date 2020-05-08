12a247c3d1f2e44f50f3c7a092b0e66e1e9d63c2
Thomas Mangin <thomas.mangin@exa.net.uk>
2020-05-08 12:26:33 +0100

    black formating of qa

--- qa/tests/control_test.py
+++ qa/tests/control_test.py
@@ -14,64 +14,66 @@ import unittest
 
 
 from exabgp.configuration.setup import environment
+
 environment.setup('')
 
 
-def speak (name, data):
-	time.sleep(0.005)
-	try:
-		sock = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
-		sock.connect(name)
-		sock.sendall(data)
-	except socket.error:
-		pass
+def speak(name, data):
+    time.sleep(0.005)
+    try:
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(name)
+        sock.sendall(data)
+    except socket.error:
+        pass
 
 
-class TestControl (unittest.TestCase):
-	def setUp (self):
-		pass
+class TestControl(unittest.TestCase):
+    def setUp(self):
+        pass
 
-	def test_failed_creation (self):
-		pass
-		# control = Control()
-		# try:
-		# 	result = control.init()
-		# 	self.assertFalse(result)
-		# except IOError:
-		# 	# could not write in the location
-		# 	pass
-		# finally:
-		# 	control.cleanup()
+    def test_failed_creation(self):
+        pass
+        # control = Control()
+        # try:
+        # 	result = control.init()
+        # 	self.assertFalse(result)
+        # except IOError:
+        # 	# could not write in the location
+        # 	pass
+        # finally:
+        # 	control.cleanup()
 
-	def validate (self, message, check):
-		pass
-		# name = tempfile.mktemp()
-		# control = Control(name,False)
-		# try:
-		# 	result = control.init()
-		# 	self.assertTrue(result)
-		#
-		# 	p = Process(target=speak, args=(name,message))
-		# 	p.start()
-		#
-		# 	string = control.loop()
-		# 	self.assertEqual(string, check)
-		# 	p.join()
-		# finally:
-		# 	control.cleanup()
-		# 	del control
+    def validate(self, message, check):
+        pass
+        # name = tempfile.mktemp()
+        # control = Control(name,False)
+        # try:
+        # 	result = control.init()
+        # 	self.assertTrue(result)
+        #
+        # 	p = Process(target=speak, args=(name,message))
+        # 	p.start()
+        #
+        # 	string = control.loop()
+        # 	self.assertEqual(string, check)
+        # 	p.join()
+        # finally:
+        # 	control.cleanup()
+        # 	del control
 
-	def test_no_newline (self):
-		self.validate('x','')
+    def test_no_newline(self):
+        self.validate('x', '')
 
-	def test_one_newline (self):
-		self.validate('x\n','x')
+    def test_one_newline(self):
+        self.validate('x\n', 'x')
 
-	def test_two_newline (self):
-		self.validate('-\nx\n','x')
+    def test_two_newline(self):
+        self.validate('-\nx\n', 'x')
 
-	def test_leftover (self):
-		self.validate('-\nx\n-','x')
+    def test_leftover(self):
+        self.validate('-\nx\n-', 'x')
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
