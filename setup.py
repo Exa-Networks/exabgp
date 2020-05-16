09c5df60188ac0f7096a8765f0b0b121019e8343
Thomas Mangin <thomas.mangin@exa.net.uk>
2020-05-16 11:10:07 +0100

    fix using  pip from git, fix example installation

--- setup.py
+++ setup.py
@@ -7,32 +7,36 @@ Created by Thomas Mangin on 2011-01-24.
 Copyright (c) 2009-2017 Exa Networks. All rights reserved.
 """
 
-import importlib
 import platform
 import os
 import sys
 import setuptools
 from distutils.core import setup
 
-# from setuptools.config import read_configuration
-# conf_dict = read_configuration('./setup.cfg', find_others=True)
 
-sys.path.append(os.path.join(os.getcwd(), os.path.dirname(sys.argv[0]), 'lib/exabgp'))
-exabgp_version = importlib.import_module('version')
+# less magic for readers than adding lib/exabgp to sys.path and using importlib
 
+get_version = os.path.join(os.getcwd(), os.path.dirname(sys.argv[0]), 'lib/exabgp/version.py')
+version = os.popen(f'{sys.executable} {get_version}').read()
+
+# without this sys.path change then this does fail
+# sudo -H pip install git+https://github.com/Exa-Networks/exabgp.git
+
+sys.path.append(os.path.join(os.getcwd(), os.path.dirname(sys.argv[0]), 'lib'))
 
 def filesOf(directory):
-    files = []
-    for l, d, fs in os.walk(directory):
-        if not d:
-            for f in fs:
-                files.append(os.path.join(l, f))
-    return files
+    return [
+        os.path.join(directory, fname)
+        for fname in os.listdir(directory)
+        if os.path.isfile(os.path.join(directory, fname))
+    ]
 
 
 data_files = [
     ('etc/exabgp/examples', filesOf('etc/exabgp')),
+    ('etc/exabgp/examples/run', filesOf('etc/exabgp/run')),
 ]
+
 
 if platform.system() != 'NetBSD':
     if sys.argv[-1] == 'systemd':
@@ -44,7 +48,8 @@ if 'systemd' in sys.argv:
     if os.path.exists('/lib/systemd/system'):
         data_files.append(('/lib/systemd/system', filesOf('etc/systemd')))
 
+
 setuptools.setup(
-    download_url='https://github.com/Exa-Networks/exabgp/archive/%s.tar.gz' % exabgp_version.version.split('-')[0],
+    download_url='https://github.com/Exa-Networks/exabgp/archive/%s.tar.gz' % version.split('-')[0],
     data_files=data_files,
 )
