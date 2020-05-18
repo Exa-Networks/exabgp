894da5ad0ee7ee1d4141a30614ac95c57cd18512
Thomas Mangin <thomas.mangin@exa.net.uk>
2020-05-18 21:58:57 +0100

    updating version to 4.2.7

--- lib/exabgp/version.py
+++ lib/exabgp/version.py
@@ -1,14 +1,13 @@
 import os
 
-commit = "99de31e8"
-release = "4.2.6"
+commit = "beaf4104"
+release = "4.2.7"
 json = "4.0.1"
 text = "4.0.1"
-version = os.environ.get('EXABGP_VERSION', release)
+version = os.environ.get('EXABGP_VERSION',release)
 
 # Do not change the first line as it is parsed by scripts
 
 if __name__ == '__main__':
-    import sys
-
-    sys.stdout.write(version)
+	import sys
+	sys.stdout.write(version)
