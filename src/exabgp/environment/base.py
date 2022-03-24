547739b2f80ca86cd551c95b0acee856d2cc9184
Thomas Mangin <thomas.mangin@exa.net.uk>
2022-03-24 23:48:13 +0000

    better detect exabgp root folder

--- src/exabgp/environment/base.py
+++ src/exabgp/environment/base.py
@@ -3,6 +3,10 @@ import sys
 
 APPLICATION = 'exabgp'
 
-ROOT = os.path.normpath(os.environ.get('EXABGP_ROOT', os.path.dirname(sys.argv[0])))
+ROOT = os.environ.get('EXABGP_ROOT','')
+if not ROOT:
+    ROOT = os.path.normpath(os.path.dirname(sys.argv[0]))
+if ROOT.endswith('/bin') or ROOT.endswith('/sbin'):
+    ROOT = os.path.normpath(os.path.join(ROOT,'..'))
 ETC = os.path.join(ROOT, 'etc', APPLICATION)
 ENVFILE = os.path.join(ETC, f'{APPLICATION}.env')
