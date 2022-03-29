2b708e2948aa8dc70c2f79115c81cc669f4710cf
Thomas Mangin <thomas.mangin@exa.net.uk>
2022-03-29 23:19:01 +0100

    JSON version change to 5.0.0
    We changed the version quite radically since the 4.2 stable branch
    So let's reflect this on the version until we release the first
    5.0.xx release with the new format bumping the release to 5.0.0

--- src/exabgp/version.py
+++ src/exabgp/version.py
@@ -2,8 +2,8 @@ import os
 
 commit = "99de31e8"
 release = "master"
-json = "4.0.1"
-text = "4.0.1"
+json = "5.0.0"
+text = "5.0.0"
 version = os.environ.get('EXABGP_VERSION', release)
 
 # Do not change the first line as it is parsed by scripts
