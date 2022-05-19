7496f3456b0f03f8d5ba1fa3dc264cf4b3aa038e
Thomas Mangin <thomas.mangin@exa.net.uk>
2022-05-19 19:53:02 +0100

    reformat rib code

--- src/exabgp/rib/__init__.py
+++ src/exabgp/rib/__init__.py
@@ -21,21 +21,22 @@ class RIB(object):
     def __init__(self, name, adj_rib_in, adj_rib_out, families):
         self.name = name
 
-        if name in self._cache:
-            self.incoming = self._cache[name].incoming
-            self.outgoing = self._cache[name].outgoing
-            self.incoming.families = families
-            self.outgoing.families = families
-            self.outgoing.delete_cached_family(families)
-
-            if adj_rib_out:
-                self.outgoing.resend(None, False)
-            else:
-                self.outgoing.clear()
-        else:
+        if name not in self._cache:
             self.incoming = IncomingRIB(adj_rib_in, families)
             self.outgoing = OutgoingRIB(adj_rib_out, families)
             self._cache[name] = self
+            return
+
+        self.incoming = self._cache[name].incoming
+        self.outgoing = self._cache[name].outgoing
+        self.incoming.families = families
+        self.outgoing.families = families
+        self.outgoing.delete_cached_family(families)
+
+        if adj_rib_out:
+            self.outgoing.resend(False)
+            return
+        self.outgoing.clear()
 
     def reset(self):
         self.incoming.reset()
@@ -47,5 +48,6 @@ class RIB(object):
 
     # This code was never tested ...
     def clear(self):
-        self._cache[self.name].incoming = IncomingRIB(self.incoming.cache, self._cache[self.name].incoming.families)
-        self._cache[self.name].outgoing = OutgoingRIB(self.outgoing.cache, self._cache[self.name].incoming.families)
+        families = self._cache[self.name].incoming.families
+        self._cache[self.name].incoming = IncomingRIB(self.incoming.cache, families)
+        self._cache[self.name].outgoing = OutgoingRIB(self.outgoing.cache, families)
