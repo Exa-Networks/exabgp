94b0ad8583a610c3ee6f82952d861339627953f0
Thomas Mangin <thomas.mangin@exa.net.uk>
2022-05-19 19:53:40 +0100

    fix print for action now it is a enum

--- src/exabgp/bgp/message/action.py
+++ src/exabgp/bgp/message/action.py
@@ -17,3 +17,16 @@ class Action(IntEnum):
     UNSET = 0x00
     ANNOUNCE = 0x01
     WITHDRAW = 0x02
+
+    def __str__(self):
+        return self.__format__('s')
+
+    def __format__(self, what):
+        if what == 's':
+            if self is Action.ANNOUNCE:
+                return 'announce'
+            if self is Action.WITHDRAW:
+                return 'withdraw'
+            if self is Action.UNSET:
+                return 'unset'
+        return 'invalid'
