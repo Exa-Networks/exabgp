1ff4afe0fbb558771e8438eecfd6823c67f3d589
Thomas Mangin <thomas.mangin@exa.net.uk>
2022-05-26 17:56:34 +0100

    added json testing

--- src/exabgp/application/decode.py
+++ src/exabgp/application/decode.py
@@ -50,6 +50,7 @@ def is_bgp(message):
 def setargs(sub):
     # fmt:off
     sub.add_argument('-n', '--nlri', help='the data is only the NLRI', action='store_true')
+    sub.add_argument('-u', '--update', help='the data is update', action='store_true')
     sub.add_argument('-d', '--debug', help='start the python debugger errors', action='store_true')
     sub.add_argument('-p', '--pdb', help='fire the debugger on fault', action='store_true')
     sub.add_argument('-c', '--configuration', help='configuration file(s)', type=str)
