7b0d4cba5e2bebdfd61984ab0f871b862cbd9d39
Thomas Mangin <thomas.mangin@exa.net.uk>
2022-11-28 14:52:55 +0000

    remove unused code into unused file

---
+++ src/exabgp/bgp/message/source.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+"""
+change.py
+
+Created by Thomas Mangin on 2022-10-28.
+Copyright (c) 2009-2017 Exa Networks. All rights reserved.
+License: 3-clause BSD. (See the COPYRIGHT file)
+"""
+
+
+class Source(object):
+    UNSET = 0x00
+    CONFIGURATION = 0x01
+    API = 0x02
+    NETWORK = 0x04
