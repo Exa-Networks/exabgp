72cd5d16c60c143dd1bb67ab2788073ec393a558
Thomas Mangin <thomas.mangin@exa.net.uk>
2023-10-06 13:31:47 +0100

    improve how we find the run folder

--- src/exabgp/environment/base.py
+++ src/exabgp/environment/base.py
@@ -1,12 +1,31 @@
 import os
 import sys
 
-APPLICATION = 'exabgp'
+def _find_root():
+    app_folder = 'src/exabgp/application'
+    root = os.environ.get('EXABGP_ROOT', '')
 
-ROOT = os.environ.get('EXABGP_ROOT', '')
-if not ROOT:
-    ROOT = os.path.normpath(os.path.dirname(sys.argv[0]))
-if ROOT.endswith('/bin') or ROOT.endswith('/sbin'):
-    ROOT = os.path.normpath(os.path.join(ROOT, '..'))
+    if not root:
+        root = os.path.dirname(sys.argv[0])
+    root = os.path.normpath(os.path.abspath((root)))
+
+    if root.endswith('/bin') or root.endswith('/sbin'):
+        root = os.path.normpath(os.path.join(root, '..'))
+
+    _index = root.find(app_folder)
+    if _index >= 0:
+        root = root[:_index]
+
+    if root.endswith('/'):
+        root = root[:-1]
+
+    return root
+
+
+APPLICATION = 'exabgp'
+ROOT = _find_root()
 ETC = os.path.join(ROOT, 'etc', APPLICATION)
 ENVFILE = os.path.join(ETC, f'{APPLICATION}.env')
+
+
+
