Exa-Networks/exabgp

commit f320ac917cdb12197043e094f54db20a9e1aa794

work on #1183

src/exabgp/configuration/flow/parser.py
@@ -275,10 +275,32 @@ def rate_limit(tokeniser):
 def redirect(tokeniser):
     data = tokeniser()
     count = data.count(':')
+
+    # the redirect is an IPv4
     if count == 0:
         return IP.create(data), ExtendedCommunities().add(TrafficNextHopSimpson(False))
-    if count == 1:
+
+    # the redirect is an IPv6
+    if data.startswith('[') and data.endswith(']'):
+        return IP.create(data[1:-1]), ExtendedCommunities().add(TrafficNextHopSimpson(False))
+
+    # the redirect is an ipv6:asn using []: notation
+    if count > 1:
+        if ']:' not in data:
+            raise ValueError('it looks like you tried to use an IPv6 but did not enclose it in []')
+
+        ip, asn = data.split(']:')
+        ip = ip.replace('[', '', 1)
+
+        # FIXME: should this be 2^16 ??
+        if asn >= pow(2, 32):
+            raise ValueError('asn is a 32 bits number, value too large %s' % asn)
+        return IP.create(ip), ExtendedCommunities().add(TrafficRedirectIPv6(ip, asn))
+
+    # the redirect is an ASN:RT
+    if True:  # count == 1:
         prefix, suffix = data.split(':', 1)
+
         if prefix.count('.'):
             raise ValueError(
                 'this format has been deprecated as it does not make sense and it is not supported by other vendors'
@@ -289,30 +311,17 @@ def redirect(tokeniser):
 
         if asn >= pow(2, 32):
             raise ValueError('asn is a 32 bits number, value too large %s' % asn)
+
         if asn >= pow(2, 16):
             if route_target >= pow(2, 16):
                 raise ValueError('asn is a 32 bits number, route target can only be 16 bit %s' % route_target)
             return NoNextHop, ExtendedCommunities().add(TrafficRedirectASN4(asn, route_target))
-        if route_target >= pow(2, 32):
-            raise ValueError('route target is a 32 bits number, value too large %s' % route_target)
-        return NoNextHop, ExtendedCommunities().add(TrafficRedirect(asn, route_target))
-    else:
-        explicit_v6 = ']:' in data
+        else:
+            if route_target >= pow(2, 32):
+                raise ValueError('route target is a 32 bits number, value too large %s' % route_target)
+            return NoNextHop, ExtendedCommunities().add(TrafficRedirect(asn, route_target))
 
-        # ipv4
-        if not explicit_v6 and data.count(':') == 1:
-            return IP.create(data), ExtendedCommunities().add(TrafficNextHopSimpson(False))
-
-        # ipv6 using []: notation
-        if explicit_v6:
-            ip, asn = data.split(']:')
-            ip = ip.replace('[', '', 1)
-            # FIXME: should this be 2^16 ??
-            if asn >= pow(2, 32):
-                raise ValueError('asn is a 32 bits number, value too large %s' % asn)
-            return IP.create(ip), ExtendedCommunities().add(TrafficRedirectIPv6(ip, asn))
-
-        raise ValueError('it looks like you tried to use an IPv6 but did not enclose it in []')
+    raise ValueError('it looks like you tried to use an IPv6 but did not enclose it in []')
 
 
 def redirect_next_hop(tokeniser):

commit 72d90d3e341854484317307c8522cf55bed52425

restore redirect flow parser for IPv6
Previous versions (eg. 4.1.2) accepted the IPv6 nexthop redirect
flowspec format:

> redirect <IPv6>;

The introduction of the "[IPv6]:NN" route-target format broke down the
ability to specify an IPv6 nexthop redirection. Thanks to the f320ac917c
("work on #1183") commit, the feature has been restored but the command
format has changed to "redirect [<IPv6>];"

Enclosing an IPv6 in square brackets helps in distinguishing the ':'
separator from the ':' characters from the IPv6 itself. However, the
"redirect <IPv6>;" format could be parsed correctly. Indeed, if the
parsed string has several ':' characters and no brackets, we know it
is not a route-target (ie. not "IPv4:NN", "[IPv6]:NN" nor "ASN:NN").

Restore the "redirect <IPv6>;" format in addition to
"redirect [<IPv6>]".

Fixes: f320ac917c ("work on #1183")
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

src/exabgp/configuration/flow/parser.py
@@ -275,8 +275,8 @@ def redirect(tokeniser):
     data = tokeniser()
     count = data.count(':')
 
-    # the redirect is an IPv4 nexthop
-    if count == 0:
+    # the redirect is an IPv4 or an IPv6 nexthop
+    if count == 0 or (count > 1 and '[' not in data and ']' not in data):
         return IP.create(data), ExtendedCommunities().add(TrafficNextHopSimpson(False))
 
     # the redirect is an IPv6 nexthop using [] notation
@@ -323,7 +323,7 @@ def redirect(tokeniser):
                 raise ValueError('Local administrator field is a 32 bits number, value too large %s' % nn)
             return NoNextHop, ExtendedCommunities().add(TrafficRedirect(asn, nn))
 
-    raise ValueError('it looks like you tried to use an IPv6 but did not enclose it in []')
+    raise ValueError('redirect format incorrect')
 
 
 def redirect_next_hop(tokeniser):