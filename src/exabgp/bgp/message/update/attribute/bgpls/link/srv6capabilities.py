ff94c9f2075b6bc6d920f41701c451ccf6ea8bb3
Quentin De Muynck <qdemuynck@iliad-free.fr>
2025-06-19 15:07:48 +0200

    Add support for SRv6 Capabilities TLV and SRv6 Locator TLV for BGP-LS

---
+++ src/exabgp/bgp/message/update/attribute/bgpls/link/srv6capabilities.py
@@ -0,0 +1,60 @@
+# encoding: utf-8
+"""
+srv6endx.py
+
+Created by Quentin De Muynck
+Copyright (c) 2025 Exa Networks. All rights reserved.
+"""
+
+from __future__ import annotations
+
+import json
+from struct import unpack
+
+from exabgp.bgp.message.update.attribute.bgpls.linkstate import LinkState
+from exabgp.bgp.message.update.attribute.bgpls.linkstate import BaseLS
+
+#    RFC 9514:  3.1.  SRv6 Capabilities TLV
+#     0                   1                   2                   3
+#     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#    |               Type            |          Length               |
+#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#    |             Flags             |         Reserved              |
+#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#
+#                    Figure 1: SRv6 Capabilities TLV Format
+
+
+@LinkState.register()
+class Srv6Capabilities(BaseLS):
+    TLV = 1038
+    registered_subsubtlvs = dict()
+
+    def __init__(self, flags):
+        self.flags = flags
+
+    def __repr__(self):
+        return 'flags: %s' % (self.flags)
+
+    @classmethod
+    def register(cls):
+        def register_subsubtlv(klass):
+            code = klass.TLV
+            if code in cls.registered_subsubtlvs:
+                raise RuntimeError('only one class can be registered per SRv6 End.X Sub-TLV type')
+            cls.registered_subsubtlvs[code] = klass
+            return klass
+
+        return register_subsubtlv
+
+    @classmethod
+    def unpack(cls, data):
+        flags_value = int.from_bytes(data[0:2], byteorder='big')
+        flags = {'O': flags_value & (1 << 6)}
+        return cls(flags=flags)
+
+    def json(self, compact=None):
+        return '"srv6-capabilities": ' + json.dumps({
+                'flags': self.flags,
+            }, indent=compact)
