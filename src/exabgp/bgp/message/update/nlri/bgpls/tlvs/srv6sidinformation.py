dad31b081cc0f853d2d9d592facedeca171bd57f
Quentin De Muynck <quentin.demuynck@outlook.com>
2025-06-20 14:30:08 +0200

    Add unit tests for SRv6 specific attributes

--- src/exabgp/bgp/message/update/nlri/bgpls/tlvs/srv6sidinformation.py
+++ src/exabgp/bgp/message/update/nlri/bgpls/tlvs/srv6sidinformation.py
@@ -40,7 +40,7 @@ class Srv6SIDInformation(object):
         return cls(sid)
 
     def json(self, compact=None):
-        return json.dumps({'srv6-sid': str(self.sid)}, indent=compact)
+        return '"srv6-sid": "%s"' % str(self.sid)
 
     def __eq__(self, other):
         return self.sid == other.sid
